cfae6d67cffe9f095c09a50a5f74cfa1ef56968c
Ron Madriz <ron@firstcreative.com>
2020-05-21 14:46:56 -0500

    custom post types created

---
+++ bei/includes/cpt_files/programs.php
@@ -0,0 +1,21 @@
+<?php
+add_action('init', function () {
+
+		register_extended_post_type('programs', array(
+
+				# Add the post type to the site's main RSS feed:
+				'show_in_feed' => true,
+				'has_archive'  => false,
+
+			), array(
+
+				# Override the base names used for labels:
+				'singular' => 'Program',
+				'plural'   => 'Programs',
+				'slug'     => 'programs',
+
+			));
+
+	});
+
+?>
