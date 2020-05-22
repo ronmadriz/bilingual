b0576828b274070be46d880dc7b0cb6d3a1ae82a
Ron Madriz <ron@firstcreative.com>
2020-05-22 15:54:07 -0500

    static - class test

--- static/header.php
+++ static/header.php
@@ -7,4 +7,4 @@ echo '<title>'.$page_title.'</title>'.PHP_EOL;
 echo '<meta name="robots" content="noindex, nofollow">'.PHP_EOL;
 echo '<link rel="stylesheet" href="/dist/css/style.min.css">'.PHP_EOL;
 echo '</head>'.PHP_EOL;
-echo '<body id="home">'.PHP_EOL;
+echo '<body id="page_'.$page_id.'" class="'.$page_classes.'">'.PHP_EOL;
--- static/index.php
+++ static/index.php
@@ -1,5 +1,6 @@
 <?php
-$page_title = "BEI Template";
-
+$page_title   = "BEI Template";
+$page_id      = 'home';
+$page_classes = 'home front-page';
 include ('header.php');
 include ('footer.php');
