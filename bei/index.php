e23ea9da092067db5a900d12dcc2dd23a6ec51d7
Ron Madriz <ron@firstcreative.com>
2020-05-26 11:56:38 -0500

    updates to styles & header code

--- bei/index.php
+++ bei/index.php
@@ -1,15 +1,13 @@
-<?php get_header(); ?>
 <?php
-    $pageID = get_the_id();
-    $pageCF = get_post_custom($pageID);
-?>
-   <main>
-    <div class="container">
-	    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	    <?php the_content(); ?>
-	    <?php endwhile; else: ?>
-	    <?php endif; ?>
-    </div>
-   </main>
-<?php get_footer(); ?>
-  
+$pageID = get_the_id();
+$pageCF = get_post_custom($pageID);
+
+get_header();
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		the_content();
+
+	}
+}
+get_footer();?>
