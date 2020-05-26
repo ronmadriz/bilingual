705235e314aa0ca079a9d62d1015da9926b023de
Ron Madriz <ron@firstcreative.com>
2020-05-26 12:37:08 -0500

    functions fix

--- bei/libs/menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action('after_setup_theme', 'register_my_menu');
-require get_template_directory().'/bootstrap-navwalker.php';
-
-function register_my_menu() {
-	register_nav_menu('primary', __('main-nav', 'main-nav'));
-}
--- bei/libs/responsive-image.php
+++
@@ -1,6 +0,0 @@
-<?php
-function ronmadriz_img_tag($class) {
-	$class .= ' img-responsive';
-	return $class;
-}
-add_filter('get_image_tag_class', 'ronmadriz_img_tag');
