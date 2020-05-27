46cce8aa1d6edb7bfed9faa0dd0d326e48d339cc
Ron Madriz <ron@firstcreative.com>
2020-05-27 13:57:27 -0500

    base - footer navigation

--- bei/views/global/footer/base.php
+++ bei/views/global/footer/base.php
@@ -14,17 +14,21 @@ endif;
 echo '<p>Box 35300<br>1810 Campus Way NE<br>Bothell, WA 98011-8246</p>'.PHP_EOL;
 echo '<p>+1-2534-4456-345</p>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-echo '<div class="base__links col-12 col-md-3">';?>
-<ul>
-	<li><a href="#">About</a></li>
-</ul>
-<ul>
-	<li><a href="#">Intensive English Program</a></li>
-</ul>
-<ul>
-	<li><a href="#">Corporate</a></li>
-</ul>
-<?
+echo '<div class="base__links col-12 col-md-3">'.PHP_EOL;
+echo '<ul>'.PHP_EOL;
+echo '<li><a href="#">About</a></li>'.PHP_EOL;
+echo '</ul>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '<div class="base__links col-12 col-md-3">'.PHP_EOL;
+echo '<ul>'.PHP_EOL;
+echo '<li><a href="#">Intensive English Program</a></li>'.PHP_EOL;
+echo '</ul>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '<div class="base__links col-12 col-md-3">'.PHP_EOL;
+echo '<ul>'.PHP_EOL;
+echo '<li><a href="#">Corporate</a></li>'.PHP_EOL;
+echo '</ul>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
