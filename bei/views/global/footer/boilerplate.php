f0c7eda80f8b425ecd8ba68c89dd5660a73777a5
Ron Madriz <ron@firstcreative.com>
2020-05-27 12:10:14 -0500

    footer - boilerplate - social media links

--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -2,7 +2,7 @@
 echo '<section id="boilerplate" class="boilerplate">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
-echo '<div id="social" class="text-center col-12 col-md-6 order-md-1">'.PHP_EOL;
+echo '<div id="social" class="text-center col-12 col-md-6 order-md-12">'.PHP_EOL;
 social_media_icons();
 echo '</div>'.PHP_EOL;
 echo '<div id="copyright" class="text-center col-12 col-md-6 order-md-1">'.PHP_EOL;
