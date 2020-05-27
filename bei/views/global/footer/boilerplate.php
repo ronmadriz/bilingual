048d1e04c6f2f36e7999a2fb8463d716493ee165
Ron Madriz <ron@firstcreative.com>
2020-05-27 15:56:09 -0500

    sprites

--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -2,7 +2,7 @@
 echo '<section id="boilerplate" class="boilerplate">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
-echo '<div id="social" class="boilerplate__item social col-12 col-md-6 order-md-12">'.PHP_EOL;
+echo '<div id="social" class="boilerplate__item social col-12 col-md-6 order-md-12 text-md-right">'.PHP_EOL;
 social_media_icons();
 echo '</div>'.PHP_EOL;
 echo '<div id="copyright" class="boilerplate__item copyright col-12 col-md-6 order-md-1">'.PHP_EOL;
