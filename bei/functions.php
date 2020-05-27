7317f6b2bcee08de4e9accf10a9c7a85fc068ea3
Ron Madriz <ron@firstcreative.com>
2020-05-27 12:20:58 -0500

    footer - boilerplate - social media links

--- bei/functions.php
+++ bei/functions.php
@@ -218,23 +218,23 @@ function social_media_icons() {
 	}
 	if (!empty($active_sites)) {
 
-		echo '<ul class="social-media-icons list-inline">'.PHP_EOL;
+		echo '<ul class="social__icons list-inline">'.PHP_EOL;
 		foreach ($active_sites as $active_site) {
 
 			$class = 'fab fa-'.$active_site;
 
 			if ($active_site == 'email') {
-				echo '<li class="list-inline-item">';
+				echo '<li class="social__icons--item list-inline-item">';
 				echo '<a class="email" href="mailto:'.antispambot(is_email(get_theme_mod($active_site))).'">';
-				echo 'i class="fa fa-envelope" title="';
+				echo 'i class="social__icons--link fa fa-envelope" title="';
 				_e('email icon', 'rmc_core');
 				echo '"></i>';
 				echo '</a>';
 				echo '</li>'.PHP_EOL;
 			} else {
-				echo '<li class="list-inline-item">';
+				echo '<li class="social__icons--item list-inline-item">';
 				echo '<a class="'.$active_site.'" href="'.get_theme_mod($active_site).'">';
-				echo '<i class="'.esc_attr($class).'" title="'.printf(__('%s icon', 'rmc_core'), $active_site).'"></i>';
+				echo '<i class="'.esc_attr($class).' social__icons--link " title="'.printf(__('%s icon', 'rmc_core'), $active_site).'"></i>';
 				echo '</a>';
 				echo '</li>'.PHP_EOL;
 			}
--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -2,10 +2,10 @@
 echo '<section id="boilerplate" class="boilerplate">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
-echo '<div id="social" class="text-center col-12 col-md-6 order-md-12">'.PHP_EOL;
+echo '<div id="social" class="boilerplate__item social text-center col-12 col-md-6 order-md-12">'.PHP_EOL;
 social_media_icons();
 echo '</div>'.PHP_EOL;
-echo '<div id="copyright" class="text-center col-12 col-md-6 order-md-1">'.PHP_EOL;
+echo '<div id="copyright" class="boilerplate__item copyright text-center col-12 col-md-6 order-md-1">'.PHP_EOL;
 echo '<p>&copy; Copyright '.date('Y').' '.get_bloginfo('name').'</p>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
