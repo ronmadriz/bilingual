106fb2b4d1a92b1df063fcc57c719acf9ca3d68d
Ron Madriz <ron@firstcreative.com>
2020-05-29 13:11:30 -0500

    subsites - font weight dropped

--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -6,7 +6,26 @@ echo '<div id="social" class="boilerplate__item social col-6 col-md-6">'.PHP_EOL
 social_media_icons_ft();
 echo '</div>'.PHP_EOL;
 echo '<div id="copyright" class="boilerplate__item copyright col-6 col-md-6 text-md-right">'.PHP_EOL;
-echo '<p>&copy; Copyright '.date('Y').' '.get_bloginfo('name').'</p>'.PHP_EOL;
+echo '<p>&copy; Copyright '.date('Y').' '.get_bloginfo('name');
+$foot_menu_args = array(
+	'theme_location'  => 'footer-nav',
+	'menu'            => 'footer-nav',
+	'container'       => '',
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => 'list-unstyled footer-nav',
+	'menu_id'         => 'footer-nav',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	'depth'           => 3,
+);
+wp_nav_menu($foot_menu_args);
+echo '</p>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
