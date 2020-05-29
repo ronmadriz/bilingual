7367c9dab23e2eb4835245338564c645e636b5e6
Ron Madriz <ron@firstcreative.com>
2020-05-29 10:47:28 -0500

    boilerplate - sprites

--- bei/views/components/nav/main.php
+++ bei/views/components/nav/main.php
@@ -1,5 +1,19 @@
 <?php
-$theme_location = get_bloginfo('stylesheet_directory');
+$site_logo_header = get_theme_mod('site_logo_header');
+$slogan           = get_bloginfo('description');
+$theme_location   = get_bloginfo('stylesheet_directory');
+
+if (!empty($site_logo_header)) {
+	echo '<a href="'.get_bloginfo('url').'" class="navbar-brand"><img src="'.esc_url($site_logo_header).'" alt="'.get_bloginfo('name').'" class="img-fluid"></a>'.PHP_EOL;
+
+} else {
+	echo '<a href="'.get_bloginfo('url').'" class="navbar-brand">'.get_bloginfo('name').(!empty($slogan)?'<br><small>'.$slogan.'</small>':'').'</a>'.PHP_EOL;
+}
+echo '<button class="navbar-toggler collapsed ml-auto" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Menu">'.PHP_EOL;
+echo '<span class="icon-bar"></span>'.PHP_EOL;
+echo '<span class="icon-bar"></span>'.PHP_EOL;
+echo '<span class="icon-bar"></span>'.PHP_EOL;
+echo '</button>'.PHP_EOL;
 echo '<div class="navbar-collapse" id="navigation">'.PHP_EOL;
 $main_menu_args = array(
 	'theme_location'  => '',
--- bei/views/global/header/index.php
+++ bei/views/global/header/index.php
@@ -3,23 +3,12 @@ global $post;
 $slug              = get_post($post)->post_name;
 $header_cta_top    = get_theme_mod('header_cta_top');
 $header_cta_bottom = get_theme_mod('header_cta_bottom');
-$slogan            = get_bloginfo('description');
-$site_logo_header  = get_theme_mod('site_logo_header');
 $theme_location    = get_template_directory();
 
 include ($theme_location.'/views/components/nav/subsites.php');
 echo '<nav class="navbar navbar-expand-lg">'.PHP_EOL;
 echo '<div class="container-fluid main__nav">'.PHP_EOL;
-if (!empty($site_logo_header)):
-echo '<a href="'.get_bloginfo('url').'" class="navbar-brand"><img src="'.esc_url($site_logo_header).'" alt="'.get_bloginfo('name').'" class="img-fluid"></a>'.PHP_EOL;
- else :
-echo '<a href="'.get_bloginfo('url').'" class="navbar-brand">'.get_bloginfo('name').(!empty($slogan)?'<br><small>'.$slogan.'</small>':'').'</a>'.PHP_EOL;
-endif;
-echo '<button class="navbar-toggler collapsed ml-auto" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Menu">'.PHP_EOL;
-echo '<span class="icon-bar"></span>'.PHP_EOL;
-echo '<span class="icon-bar"></span>'.PHP_EOL;
-echo '<span class="icon-bar"></span>'.PHP_EOL;
-echo '</button>'.PHP_EOL;
+
 include ($theme_location.'/views/components/nav/main.php');
 echo '</div>'.PHP_EOL;
 echo '</nav>'.PHP_EOL;
