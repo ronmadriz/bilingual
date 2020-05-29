8152e588f948d70aa717046f4a3d6d99b5310e52
Ron Madriz <ron@firstcreative.com>
2020-05-29 14:05:30 -0500

    footer - base - footer links

--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -1,11 +1,11 @@
 <?php
 echo '<section id="boilerplate" class="boilerplate">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row">'.PHP_EOL;
+echo '<div class="row align-items-center">'.PHP_EOL;
 echo '<div id="social" class="boilerplate__item social col-6 col-md-6">'.PHP_EOL;
 social_media_icons_ft();
 echo '</div>'.PHP_EOL;
-echo '<div id="copyright" class="boilerplate__item copyright col-6 col-md-6 text-md-right align-self-center">'.PHP_EOL;
+echo '<div id="copyright" class="boilerplate__item copyright col-6 col-md-6 text-md-right">'.PHP_EOL;
 echo '<p>&copy; Copyright '.date('Y').' '.get_bloginfo('name').'</p>'.PHP_EOL;
 $foot_menu_args = array(
 	'theme_location'  => 'footer-nav',
