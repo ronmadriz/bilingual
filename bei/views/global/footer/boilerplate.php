d38863affc30cdf9ce21c09ec64a67d6ec33cc22
Ron Madriz <ron@firstcreative.com>
2020-05-29 12:56:16 -0500

    font declaration

--- bei/views/global/footer/boilerplate.php
+++ bei/views/global/footer/boilerplate.php
@@ -2,10 +2,10 @@
 echo '<section id="boilerplate" class="boilerplate">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
-echo '<div id="social" class="boilerplate__item social col-6 col-md-6 order-md-12 text-md-right">'.PHP_EOL;
+echo '<div id="social" class="boilerplate__item social col-6 col-md-6 text-md-right">'.PHP_EOL;
 social_media_icons_ft();
 echo '</div>'.PHP_EOL;
-echo '<div id="copyright" class="boilerplate__item copyright col-6 col-md-6 order-md-1">'.PHP_EOL;
+echo '<div id="copyright" class="boilerplate__item copyright col-6 col-md-6">'.PHP_EOL;
 echo '<p>&copy; Copyright '.date('Y').' '.get_bloginfo('name').'</p>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
