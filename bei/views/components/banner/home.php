1775243fe48f23d75c631b2ad347f99093725c78
Ron Madriz <ron@firstcreative.com>
2020-06-02 01:47:07 -0500

    homepage - banner

--- bei/views/components/banner/home.php
+++ bei/views/components/banner/home.php
@@ -1,18 +1,20 @@
 <?php ?>
-<section id="banner">
+<section id="banner" class="banner">
   <div class="container-fluid">
-    <div class="row">
-		<div id="bannerSlides" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner row" role="listbox">
-				<figure class="carousel-item">
-					<img src="https://demo.goodlayers.com/kingster/wp-content/uploads/2018/08/slider-1-2.jpg" alt="" class="img-fluid mx-auto my-auto d-block">
-					<figcaption class=""></figcaption>
-				</figure>
-			</div>
-			<a class="carousel-control-prev" href="#bannerSlides" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
-			<a class="carousel-control-next" href="#bannerSlides" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>
+	<div id="bannerSlides" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner row" role="listbox">
+			<figure class="carousel-item">
+				<img src="https://demo.goodlayers.com/kingster/wp-content/uploads/2018/08/slider-1-2.jpg" alt="" class="img-fluid banner__image">
+				<figcaption class="banner__caption">
+					<span class="banner__subtitle subtitle"></span>
+					<h2 class="banner__title title"></h2>
+					<a class="banner__button d-inline-block" href="#">Take a Tour</a>
+				</figcaption>
+			</figure>
 		</div>
-    </div>
+		<a class="carousel-control-prev" href="#bannerSlides" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
+		<a class="carousel-control-next" href="#bannerSlides" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>
+	</div>
   </div>
 </section>
 <?php ?>
