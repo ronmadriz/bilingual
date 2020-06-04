d6e59d8c817ebf7f1b4516f4bd8edf3424f9fc97
Ron Madriz <ron@firstcreative.com>
2020-06-03 23:21:38 -0500

    banner - vertical align caption

--- bei/views/components/banner/home.php
+++ bei/views/components/banner/home.php
@@ -7,7 +7,7 @@ echo '<div id="banner__slides" class="carousel slide" data-ride="carousel">'.PHP
 echo '<div class="carousel-inner row banner__slides" role="listbox">'.PHP_EOL;
 echo '<figure class="carousel-item banner__slide active">'.PHP_EOL;
 echo '<img src="https://demo.goodlayers.com/kingster/wp-content/uploads/2018/08/slider-1-2.jpg" alt="" class="img-fluid banner__image">'.PHP_EOL;
-echo '<figcaption class="banner__caption">'.PHP_EOL;
+echo '<figcaption class="banner__caption d-flex h-100 align-items-center">'.PHP_EOL;
 echo '<span class="banner__subtitle subtitle text-capitalize">Kingster has more than</span>'.PHP_EOL;
 echo '<h2 class="banner__title title">180 Majors &amp; Minors</h2>'.PHP_EOL;
 echo '<a class="banner__button d-inline-block" href="#">Take a Tour</a>'.PHP_EOL;
@@ -15,7 +15,7 @@ echo '</figcaption>'.PHP_EOL;
 echo '</figure>'.PHP_EOL;
 echo '<figure class="carousel-item banner__slide">'.PHP_EOL;
 echo '<img src="https://demo.goodlayers.com/kingster/wp-content/uploads/2018/08/slider-1-2.jpg" alt="" class="img-fluid banner__image">'.PHP_EOL;
-echo '<figcaption class="banner__caption">'.PHP_EOL;
+echo '<figcaption class="banner__caption d-flex h-100 align-items-center">'.PHP_EOL;
 echo '<span class="banner__subtitle subtitle text-capitalize">The best university of the state</span>'.PHP_EOL;
 echo '<h2 class="banner__title title">Kingster University</h2>'.PHP_EOL;
 echo '<a class="banner__button d-inline-block" href="#">Take a Tour</a>'.PHP_EOL;
