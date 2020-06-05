5dcbf2c543e1834e4c360a7894d7b055be39a400
Ron Madriz <ron@firstcreative.com>
2020-06-05 17:29:35 -0500

    new component - icon strip

--- bei/views/pages/home.php
+++ bei/views/pages/home.php
@@ -2,3 +2,4 @@
 // home
 include (get_template_directory().'/views/components/banner/home.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
+include (get_template_directory().'/views/components/layout/icons.php');
