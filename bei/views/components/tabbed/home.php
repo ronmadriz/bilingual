8d0d7c8b40a9ec3c34019641723ff432188276d3
Ron Madriz <ron@firstcreative.com>
2020-06-04 22:32:10 -0500

    tabbed details - h3

--- bei/views/components/tabbed/home.php
+++ bei/views/components/tabbed/home.php
@@ -35,8 +35,8 @@ if (have_rows('tab')) {
 				echo '<span class="tabbed__image"><img src="'.$tab__image['url'].'" alt="'.$tab__image['alt'].'" class="img-fluid"></span>'.PHP_EOL;
 				echo '<span class="tabbed__details">'.PHP_EOL;
 				echo $tab__content.PHP_EOL;
-				echo '</span>'.PHP_EOL;
 				echo (!empty($tab__button)?'<a class="tabbed__button button__green" href="'.$tab__button['url'].'">'.$tab__button['title'].'</a>'.PHP_EOL:'');
+				echo '</span>'.PHP_EOL;
 				echo '</div>'.PHP_EOL;
 			}
 			wp_reset_query();
