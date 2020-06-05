50abf474feb5f97468c18835d9ee319a6a3a3b5b
Ron Madriz <ron@firstcreative.com>
2020-06-05 12:59:22 -0500

    update programs

--- bei/includes/cpt_files/programs.php
+++ bei/includes/cpt_files/programs.php
@@ -1,21 +1,24 @@
 <?php
 add_action('init', function () {
-
 		register_extended_post_type('programs', array(
-
 				# Add the post type to the site's main RSS feed:
-				'show_in_feed' => true,
-				'has_archive'  => false,
-				'menu_icon'    => 'dashicons-networking',
+				'show_in_feed'      => true,
+				'has_archive'       => false,
+				'menu_icon'         => 'dashicons-networking',
+				'show_in_nav_menus' => true,
+				'hierarchical'      => true,
+				'public'            => true,
+				'supports'          => array(
+					'page-attributes'/* This will show the post parent field */,
+					'title',
+					'editor',
+					'thumbnail',
+				),
 			), array(
-
 				# Override the base names used for labels:
 				'singular' => 'Program',
 				'plural'   => 'Programs',
 				'slug'     => 'programs',
-
 			));
-
 	});
-
 ?>
---
+++ bei/views/layout/icons.php
@@ -0,0 +1,10 @@
+<?php
+echo '<section id="icons">'.PHP_EOL;
+echo '<div class="container-fluid">'.PHP_EOL;
+echo '<div class="row">'.PHP_EOL;
+echo '<div class="col-6 col-md-3">'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
