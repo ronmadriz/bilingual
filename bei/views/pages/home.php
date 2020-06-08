93c966ffbd046005bce2460924aa09da8ae38d79
Ron Madriz <ron@firstcreative.com>
2020-06-08 03:17:59 -0500

    icons

--- bei/views/pages/home.php
+++ bei/views/pages/home.php
@@ -2,4 +2,4 @@
 // home
 include (get_template_directory().'/views/components/banner/home.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
-include (get_template_directory_uri().'/views/icons/icons.php');
+include (get_template_directory().'/views/components/icons/icons.php');
