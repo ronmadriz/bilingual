353adf64468b878c65f880f6b299322a499da7f8
Ron Madriz <ron@firstcreative.com>
2020-06-08 12:02:12 -0500

    icon - homepage - description styling

--- bei/views/components/icons/icons.php
+++ bei/views/components/icons/icons.php
@@ -10,7 +10,8 @@ if (have_rows('icons')) {
 		$icons_desc   = get_sub_field('desc');
 		$icons_button = get_sub_field('button');
 		echo '<div class="icons__item col-6 col-md-3">'.PHP_EOL;
-		echo '<img class="icons__img" src="'.get_stylesheet_directory_uri().'/sprites/'.$icons_icon.'.svg" alt="'.$icons_icon.'" class="img-fluid">'.PHP_EOL;
+		// echo '<img class="icons__img" src="'.get_stylesheet_directory_uri().'/sprites/'.$icons_icon.'.svg" alt="'.$icons_icon.'" class="img-fluid">'.PHP_EOL;
+		echo file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$icons_icon.'.svg');
 		echo '<h3 class="icons__title">'.$icons_title.'</h3>'.PHP_EOL;
 		echo '<p class="icons__desc">'.$icons_desc.'</p>'.PHP_EOL;
 		echo (!empty($icons_button)?'<a class="icons__button" href="'.$icons_button['url'].'">'.$icons_button['title'].'</a>'.PHP_EOL:'');
