02432f8fcd0a1521b00d72d58cb78f74e0341d2e
Ron Madriz <ron@firstcreative.com>
2020-06-08 15:29:14 -0500

    vidtabs - structure

--- bei/views/components/tabbed/vidTabs.php
+++ bei/views/components/tabbed/vidTabs.php
@@ -3,19 +3,19 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12 col-md-9 order-md-12">
-				<ul class="nav nav-tabs" id="myTab" role="tablist">
-				  <li class="nav-item"><a class="nav-link active" id="vidTab-1-tab" data-toggle="tab" href="#vidTab-1" role="tab" aria-controls="vidTab-1" aria-selected="true">Home</a></li>
-				  <li class="nav-item"><a class="nav-link" id="vidTab-2-tab" data-toggle="tab" href="#vidTab-2" role="tab" aria-controls="vidTab-2" aria-selected="false">Profile</a></li>
-				  <li class="nav-item"><a class="nav-link" id="vidTab-3-tab" data-toggle="tab" href="#vidTab-3" role="tab" aria-controls="vidTab-3" aria-selected="false">Contact</a></li>
+				<ul class="nav nav-tabs vidTab__list" role="tablist">
+				  <li class="nav-item vidTab__item"><a class="vidTab__link nav-link active" id="vidTab-1-tab" data-toggle="tab" href="#vidTab-1" role="tab" aria-controls="vidTab-1" aria-selected="true">Home</a></li>
+				  <li class="nav-item vidTab__item"><a class="vidTab__link nav-link" id="vidTab-2-tab" data-toggle="tab" href="#vidTab-2" role="tab" aria-controls="vidTab-2" aria-selected="false">Profile</a></li>
+				  <li class="nav-item vidTab__item"><a class="vidTab__link nav-link" id="vidTab-3-tab" data-toggle="tab" href="#vidTab-3" role="tab" aria-controls="vidTab-3" aria-selected="false">Contact</a></li>
 				</ul>
-				<div class="tab-content" id="myTabContent">
-				  <div class="tab-pane fade show active" id="vidTab-1" role="tabpanel" aria-labelledby="vidTab-1-tab">Raw denim you
+				<div class="tab-content vidTab__content">
+				  <div class="tab-pane fade show active vidTab__pane" id="vidTab-1" role="tabpanel" aria-labelledby="vidTab-1-tab">Raw denim you
 				    probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master
 				    cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
 				    keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
 				    placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi
 				    qui.</div>
-				  <div class="tab-pane fade" id="vidTab-2" role="tabpanel" aria-labelledby="vidTab-2-tab">Food truck fixie
+				  <div class="tab-pane fade vidTab__pane" id="vidTab-2" role="tabpanel" aria-labelledby="vidTab-2-tab">Food truck fixie
 				    locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
 				    blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
 				    Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
@@ -23,7 +23,7 @@
 				    salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
 				    sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
 				    stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</div>
-				  <div class="tab-pane fade" id="vidTab-3" role="tabpanel" aria-labelledby="vidTab-3-tab">Etsy mixtape
+				  <div class="tab-pane fade vidTab__pane" id="vidTab-3" role="tabpanel" aria-labelledby="vidTab-3-tab">Etsy mixtape
 				    wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
 				    lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard
 				    locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify
