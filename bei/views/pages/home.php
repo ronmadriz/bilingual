08ddbd2b6e0885148377a2c77e7a3447d43455e0
Ron Madriz <ron@firstcreative.com>
2020-06-08 15:06:29 -0500

    vidtabs

--- bei/views/pages/home.php
+++ bei/views/pages/home.php
@@ -3,3 +3,4 @@
 include (get_template_directory().'/views/components/banner/home.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
 include (get_template_directory().'/views/components/icons/icons.php');
+include (get_template_directory().'/views/components/tabbed/vidTabs.php');
