4488920e17f28a226085bc9251cf738fea849d2a
Ron Madriz <ron@firstcreative.com>
2020-06-09 11:39:55 -0500

    tabbed - background

--- bei/views/components/logos/logos.php
+++ bei/views/components/logos/logos.php
@@ -1,8 +1,18 @@
 <?php
-echo '<section id="logos" class="logos">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row align-content-center text-center">'.PHP_EOL;
-echo '<div class="col-6 col-md-3 logos_item"><img src="'.get_template_directory_uri().'/logos/accet.gif" alt="Accet" class="logos_image img-fluid"></div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
+if (have_rows('partner_logo_strip')) {
+	echo '<section id="partner_partner_logos" class="partner_partner_logos">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	while (have_rows('partner_logo_strip')) {
+		the_row();
+		echo '<div class="row align-content-center text-center">'.PHP_EOL;
+		$partner_logos = get_sub_field('logos');
+		if ($partner_logos) {
+			foreach ($partner_logos as $partner_logo) {
+				echo '<div class="col-6 col-md-3 partner_partner_logos_item"><img src="'.$partner_logo['url'].'" alt="'.$partner_logo['alt'].'" class="logos_image img-fluid"></div>'.PHP_EOL;
+			}
+		}
+		echo '</div>'.PHP_EOL;
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
