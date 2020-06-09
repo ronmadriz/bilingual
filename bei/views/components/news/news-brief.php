acd1518732b9a505e77e24185e98444e10a4e8c8
Ron Madriz <ron@firstcreative.com>
2020-06-09 16:40:25 -0500

    news - featured post - details fix - date

--- bei/views/components/news/news-brief.php
+++ bei/views/components/news/news-brief.php
@@ -19,9 +19,9 @@ if ($featured_brief_query->have_posts()) {
 	while ($featured_brief_query->have_posts()) {
 		$featured_brief_query->the_post();
 		echo '<div class="brief__featured col-12 col-md-6">'.PHP_EOL;
-		echo '<span class="brief__image"><img src="https://via.placeholder.com/640x480" alt="featured story image" class="img-fluid"></span>'.PHP_EOL;
-		echo '<span class="brief__details"><date class="brief__date">'.get_the_date().'</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>'.PHP_EOL;
-		echo '<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>'.PHP_EOL;
+		echo '<span class="brief__image"><a href="'.get_the_permalink().'"><img src="https://via.placeholder.com/640x480" alt="featured story image" class="img-fluid"></a></span>'.PHP_EOL;
+		echo '<span class="brief__details"><date class="brief__date">'.get_the_date().'</date> <a href="#" class="brief__category">'.get_the_category().'</a>, <a href="#" class="brief__author">STUDENT</a></span>'.PHP_EOL;
+		echo '<h3 class="brief__title"><a href="'.get_the_permalink().'" class="brief__link">'.get_the_title().'</a></h3>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	rewind_posts();
