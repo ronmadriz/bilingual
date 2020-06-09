aee8e3a9f172945bf4cac6e4b160a0c39de17379
Ron Madriz <ron@firstcreative.com>
2020-06-09 14:35:51 -0500

    news - quick links

--- bei/views/components/news/news-brief.php
+++ bei/views/components/news/news-brief.php
@@ -13,19 +13,19 @@ echo '<div id="brief" class="brief col-12 col-md-8">'.PHP_EOL;
 	</div>
 	<div class="brief__list col-12 col-md-6">
 		<article class="brief__item">
-			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
-			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
-			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
+			<span class="brief__image brief__item--image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__details brief__item--details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
+			<h3 class="brief__title brief__item--title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
 		<article class="brief__item">
-			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
-			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
-			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
+			<span class="brief__image brief__item--image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__details brief__item--details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
+			<h3 class="brief__title brief__item--title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
 		<article class="brief__item">
-			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
-			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
-			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
+			<span class="brief__image brief__item--image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__details brief__item--details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
+			<h3 class="brief__title brief__item--title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
 	</div>
 </div>
