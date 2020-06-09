f772d12e3385ce6eff2f27e49564fb444bb1110c
Ron Madriz <ron@firstcreative.com>
2020-06-08 23:45:17 -0500

    logos - strip component

---
+++ bei/styles/components/logos/logos.php
@@ -0,0 +1,13 @@
+<?php ?>
+<section id="logos">
+	<div class="container-fluid">
+		<div class="row align-content-center justify-content-center">
+			<div class="col-12">
+				<img src="logos/accet.gif" alt="Accet" class="logos_image">
+				<img src="logos/english-usa.gif" alt="English USA" class="logos_image">
+				<img src="logos/nafsa.gif" alt="NAFSA" class="logos_image">
+				<img src="logos/tesol.gif" alt="Tesol" class="logos_image">
+			</div>
+		</div>
+	</div>
+</section>
