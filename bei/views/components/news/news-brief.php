eeed79535b9f19daa5cb65f7c32bbd7dc910c02e
Ron Madriz <ron@firstcreative.com>
2020-06-09 13:53:29 -0500

    news - quick links

--- bei/views/components/news/news-brief.php
+++ bei/views/components/news/news-brief.php
@@ -12,17 +12,17 @@ echo '<div id="brief" class="brief col-12 col-md-9">'.PHP_EOL;
 	</div>
 	<div class="brief__list col-12 col-md-6">
 		<article>
-			<span class="brief__image"><img src="https://via.placeholder.com/640x480" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
 			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
 			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
 		<article>
-			<span class="brief__image"><img src="https://via.placeholder.com/640x480" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
 			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
 			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
 		<article>
-			<span class="brief__image"><img src="https://via.placeholder.com/640x480" alt="featured story image" class="img-fluid"></span>
+			<span class="brief__image"><img src="https://via.placeholder.com/90" alt="featured story image" class="img-fluid"></span>
 			<span class="brief__details"><date class="brief__date">JUNE 6, 2016</date> <a href="#" class="brief__category">ADMISSION</a>, <a href="#" class="brief__author">STUDENT</a></span>
 			<h3 class="brief__title"><a href="#" class="brief__link">Professor Albert joint research on mobile money in Tanzania</a></h3>
 		</article>
