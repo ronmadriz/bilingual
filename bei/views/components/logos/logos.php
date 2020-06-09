ee57edb29f4a550e232c748b82b9e607784706b5
Ron Madriz <ron@firstcreative.com>
2020-06-09 00:07:37 -0500

    logos - strip component

--- bei/views/components/logos/logos.php
+++ bei/views/components/logos/logos.php
@@ -1,8 +1,8 @@
 <?php
-echo '<section id="logos">'.PHP_EOL;
+echo '<section id="logos" class="logos">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row align-content-center text-center">'.PHP_EOL;
-echo '<div class="col-12">'.PHP_EOL;
+echo '<div class="col-12 logos_strip">'.PHP_EOL;
 echo '<img src="'.get_template_directory_uri().'/logos/accet.gif" alt="Accet" class="logos_image img-fluid">'.PHP_EOL;
 echo '<img src="'.get_template_directory_uri().'/logos/english-usa.gif" alt="English USA" class="logos_image img-fluid">'.PHP_EOL;
 echo '<img src="'.get_template_directory_uri().'/logos/nafsa.gif" alt="NAFSA" class="logos_image img-fluid">'.PHP_EOL;
