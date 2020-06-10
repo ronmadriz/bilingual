f946bc1b71bfade985628f33a0289c11d801c4a5
Ron Madriz <ron@firstcreative.com>
2020-06-10 03:51:45 -0500

    vidtabbed mobile

--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -5,9 +5,12 @@ if (have_posts()) {
 	echo '<div class="row">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
-		the_title();
-		the_content();
+		echo '<div class="col-12">'.PHP_EOL;
+		echo '<h1 class="summary__title">'.get_the_title().'</h1>'.PHP_EOL;
+		echo '<div class="summary__content">'.get_the_content().'</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
 	}
+	wp_reset_postdata();
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
