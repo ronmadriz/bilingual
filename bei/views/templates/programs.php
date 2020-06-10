72d29778f1789a96c635ce77ed9d782fe6bb46ed
Ron Madriz <ron@firstcreative.com>
2020-06-10 03:35:43 -0500

    vidtabbed mobile

--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -1,8 +1,14 @@
 <?php
 if (have_posts()) {
+	echo '<section id="summary" class="summary">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
 		the_title();
 		the_content();
 	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
 }
