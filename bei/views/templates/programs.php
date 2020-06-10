f791faa56f243b7d22d33aca6f22bf4f2548272f
Ron Madriz <ron@firstcreative.com>
2020-06-10 17:21:34 -0500

    Programs - Subject

--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -25,10 +25,9 @@ if (have_rows('subjects')) {
 		$subject_title = get_sub_field('title');
 		$subject_desc  = get_sub_field('description');
 		echo '<div class="col-6 col-md-3">'.PHP_EOL;
-		echo '<span class="icons__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$icons_icon.'.svg').'</span>'.PHP_EOL;
-		echo '<h3 class="icons__title">'.$icons_title.'</h3>'.PHP_EOL;
-		echo '<p class="icons__desc">'.$icons_desc.'</p>'.PHP_EOL;
-		echo (!empty($icons_button)?'<a class="icons__button" href="'.$icons_button['url'].'">'.$icons_button['title'].'</a>'.PHP_EOL:'');
+		echo '<span class="icons__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$subject_icon.'.svg').'</span>'.PHP_EOL;
+		echo '<h3 class="icons__title">'.$subject_title.'</h3>'.PHP_EOL;
+		echo '<p class="icons__desc">'.$subject_desc.'</p>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
