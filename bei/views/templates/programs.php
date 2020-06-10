ae32164b6d34919a46fc8c8e03ba11d9572df15d
Ron Madriz <ron@firstcreative.com>
2020-06-10 17:23:26 -0500

    Programs - Subject

--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -24,10 +24,10 @@ if (have_rows('subjects')) {
 		$subject_icon  = get_sub_field('icon');
 		$subject_title = get_sub_field('title');
 		$subject_desc  = get_sub_field('description');
-		echo '<div class="col-6 col-md-3">'.PHP_EOL;
-		echo '<span class="icons__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$subject_icon.'.svg').'</span>'.PHP_EOL;
-		echo '<h3 class="icons__title">'.$subject_title.'</h3>'.PHP_EOL;
-		echo '<p class="icons__desc">'.$subject_desc.'</p>'.PHP_EOL;
+		echo '<div class="subjects__item col-6 col-md-3">'.PHP_EOL;
+		echo '<span class="subjects__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$subject_icon.'.svg').'</span>'.PHP_EOL;
+		echo '<h3 class="subjects__title">'.$subject_title.'</h3>'.PHP_EOL;
+		echo '<p class="subjects__desc">'.$subject_desc.'</p>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
