cc320d016bac2651805c69eadf34cc4a66b7b2d5
Ron Madriz <ron@firstcreative.com>
2020-06-11 16:56:37 -0500

    page title fix

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -4,13 +4,13 @@ if (have_posts()) {
 	echo '<div class="container-fluid">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
-		echo '<div class="row"><div class="page__title col-12 col-md-8"><h1 class="main__title">'.get_the_title().'</h1></div></div>'.PHP_EOL;
+		echo '<div class="row"><div class="col-12 col-md-8"><h1 class="main__title">'.get_the_title().'</h1></div></div>'.PHP_EOL;
 		echo '<div class="row"><div class="col-12 col-md-8 breadcrumbs__content">'.PHP_EOL;
 		the_breadcrumb();
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
-		echo '<div class="row"><div class="page__content main__content col-12 col-md-4">'.get_the_content().'</div></div>'.PHP_EOL;
-		echo '<div class="row"><div class="page__content main__content col-12 col-md-8>'.get_the_content().'</div></div>'.PHP_EOL;
+		echo '<div class="row"><div class="main__thumbnail col-12 col-md-4">'.get_the_content().'</div></div>'.PHP_EOL;
+		echo '<div class="row"><div class="main__content col-12 col-md-8>'.get_the_content().'</div></div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
