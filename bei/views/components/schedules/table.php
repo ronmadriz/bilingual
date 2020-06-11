237f4ebd715d9b2b8af15dc5652009ca4df95b75
Ron Madriz <ron@firstcreative.com>
2020-06-11 00:27:23 -0500

    Programs - Subject

---
+++ bei/views/components/schedules/table.php
@@ -0,0 +1,18 @@
+<?php
+// Used on Program Schedules
+
+echo '<section id="schedule" class="schedule">'.PHP_EOL;
+echo '<div class="container-fluid">'.PHP_EOL;
+echo '<div class="row">'.PHP_EOL;
+echo '<div class="col-12">'.PHP_EOL;
+echo '<table class="table">'.PHP_EOL;
+echo '<thead>'.PHP_EOL;
+echo '<tr><th>';
+_e('Cycle # amp&; Date');
+echo '</th></tr>'.PHP_EOL;
+echo '</thead>'.PHP_EOL;
+echo '</table>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -2,3 +2,4 @@
 include (get_template_directory().'/views/global/main.php');
 include (get_template_directory().'/views/components/icons/subjects.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
+include (get_template_directory().'/views/components/schedules/table.php');
