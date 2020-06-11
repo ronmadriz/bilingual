2591190dcec8a7bbddd00c7df4ae3d1515f42206
Ron Madriz <ron@firstcreative.com>
2020-06-11 02:47:13 -0500

    logo - schedules upload

--- bei/views/templates/programs/tabbed.php
+++ bei/views/templates/programs/tabbed.php
@@ -3,7 +3,7 @@ echo '<section id="tabbed" class="tabbed">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row justify-content-end">'.PHP_EOL;
 echo '<div class="tabbed__wrapper col-12 col-md-10">'.PHP_EOL;
-include ('../components/tabbed/simple.php');
+include (get_template_directory().'/views/components/tabbed/simple.php');
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
