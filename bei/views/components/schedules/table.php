194863d89291159c26933ce04ffcc33c80ac1ddf
Ron Madriz <ron@firstcreative.com>
2020-06-11 01:16:45 -0500

    logo - schedules upload

--- bei/views/components/schedules/table.php
+++ bei/views/components/schedules/table.php
@@ -21,13 +21,15 @@ if (have_rows('schedules')) {
 		_e('End Date');
 		echo '</th></tr>'.PHP_EOL;
 		echo '</thead>'.PHP_EOL;
-		echo '<tbody>'.PHP_EOL;
-		echo '<tr>'.PHP_EOL;
-		echo '<td>Value 1</td>'.PHP_EOL;
-		echo '<td>Value 2</td>'.PHP_EOL;
-		echo '<td>Value 3</td>'.PHP_EOL;
-		echo '</tr>'.PHP_EOL;
-		echo '</tbody>'.PHP_EOL;
+		if (have_rows('schedule')) {
+			echo '<tbody>'.PHP_EOL;
+			echo '<tr>'.PHP_EOL;
+			echo '<td>Value 1</td>'.PHP_EOL;
+			echo '<td>Value 2</td>'.PHP_EOL;
+			echo '<td>Value 3</td>'.PHP_EOL;
+			echo '</tr>'.PHP_EOL;
+			echo '</tbody>'.PHP_EOL;
+		}
 		echo '</table>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
