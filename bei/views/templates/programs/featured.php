b99b409a2a2a56b0b61257ec181cdae50d02dc3e
Ron Madriz <ron@firstcreative.com>
2020-06-11 12:35:09 -0500

    programs - schedule - layout

--- bei/views/templates/programs/featured.php
+++ bei/views/templates/programs/featured.php
@@ -9,7 +9,7 @@ if (have_rows('featured')) {
 		$featured_item_content = get_sub_field('featured_item_content');
 		echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
 		echo '<h3 class="featured__title">'.$featured_item_title.'</h3>'.PHP_EOL;
-		echo '<span class="featured__desc">'.$featured_item_title.'</span>'.PHP_EOL;
+		echo '<span class="featured__desc">'.$featured_item_content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
