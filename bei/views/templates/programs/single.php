326aeebb80370740ad9cee1ce4fbe55cc0115480
Ron Madriz <ron@firstcreative.com>
2020-06-11 12:39:56 -0500

    programs - schedule - layout

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -1,6 +1,6 @@
 <?php
 include (get_template_directory().'/views/global/main.php');
-include (get_template_directory().'/views/components/icons/subjects.php');
+include ('subjects.php');
 include ('tabs.php');
 include (get_template_directory().'/views/components/schedules/table.php');
 include ('featured.php');
---
+++ bei/views/templates/programs/subjects.php
@@ -0,0 +1,10 @@
+<?php
+if (have_rows('subjects')) {
+	echo '<section id="subjects" class="subjects">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	include (get_template_directory().'/views/components/icons/subjects.php');
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
