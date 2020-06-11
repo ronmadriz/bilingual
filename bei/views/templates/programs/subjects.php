ef10b4d210b17ec74d6afc49086c483ca7c381af
Ron Madriz <ron@firstcreative.com>
2020-06-11 18:22:06 -0500

    page title fix

--- bei/views/templates/programs/subjects.php
+++ bei/views/templates/programs/subjects.php
@@ -2,7 +2,7 @@
 if (have_rows('subjects')) {
 	echo '<section id="subjects" class="subjects">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row"><div class="col-12"><h2>';
+	echo '<div class="row"><div class="col-12 subjects__section"><h2>';
 	_e('Core Subjects', 'bei_core');
 	echo '</h2></div></div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
