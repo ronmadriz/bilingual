1935f4c1e72bbf3edca2b54ce87e63e1584b3dbe
Ron Madriz <ron@firstcreative.com>
2020-06-11 02:54:18 -0500

    logo - schedules upload

--- bei/views/components/tabbed/simple.php
+++ bei/views/components/tabbed/simple.php
@@ -4,27 +4,27 @@ if (have_rows('simple_tabs')) {
 		the_row();
 		$tab__item = get_sub_field('tab__item');
 		if ($tab__item) {
-			echo '<ul class="tabbed__items nav nav-pills nav-justified">'.PHP_EOL;
+			echo '<ul class="simple_tabs__items nav nav-pills nav-justified">'.PHP_EOL;
 			$tab__count = 0;
 			while (have_rows('tab__item')) {
 				the_row();
 				$tab__title    = get_sub_field('tab__title');
 				$tab__subtitle = get_sub_field('tab__subtitle');
-				echo '<li class="nav-item tabbed__item"><a class="nav-link tabbed__button'.($tab__count == 0?' active':'').'" data-toggle="tab" href="#home__tab-'.$tab__count++ .'">'.PHP_EOL;
-				echo '<span class="tabbed__text">'.PHP_EOL;
-				echo '<h3 class="tabbed__title">'.$tab__title.'</h3>'.PHP_EOL;
-				echo '<p class="tabbed__subtitle">'.$tab__subtitle.'</p>'.PHP_EOL;
+				echo '<li class="nav-item simple_tabs__item"><a class="nav-link simple_tabs__button'.($tab__count == 0?' active':'').'" data-toggle="tab" href="#home__tab-'.$tab__count++ .'">'.PHP_EOL;
+				echo '<span class="simple_tabs__text">'.PHP_EOL;
+				echo '<h3 class="simple_tabs__title">'.$tab__title.'</h3>'.PHP_EOL;
+				echo '<p class="simple_tabs__subtitle">'.$tab__subtitle.'</p>'.PHP_EOL;
 				echo '</span></a></li>'.PHP_EOL;
 			}
 			wp_reset_query();
 			echo '</ul>'.PHP_EOL;
-			echo '<div class="tabbed__content tab-content">'.PHP_EOL;
+			echo '<div class="simple_tabs__content tab-content">'.PHP_EOL;
 			$tabpanel__count = 0;
 			while (have_rows('tab__item')) {
 				the_row();
 				$tab__content = get_sub_field('tab__content');
-				echo '<div class="tab-pane row tabbed__pane fade show'.($tabpanel__count == 0?' active':'').'" id="home__tab-'.$tabpanel__count++ .'" role="tabpanel">'.PHP_EOL;
-				echo '<div class="tabbed__details order-12">'.PHP_EOL;
+				echo '<div class="tab-pane row simple_tabs__pane fade show'.($tabpanel__count == 0?' active':'').'" id="home__tab-'.$tabpanel__count++ .'" role="tabpanel">'.PHP_EOL;
+				echo '<div class="simple_tabs__details order-12">'.PHP_EOL;
 				echo $tab__content.PHP_EOL;
 				echo '</div>'.PHP_EOL;
 				echo '</div>'.PHP_EOL;
--- bei/views/templates/programs/tabbed.php
+++ bei/views/templates/programs/tabbed.php
@@ -1,8 +1,8 @@
 <?php
-echo '<section id="tabbed" class="tabbed">'.PHP_EOL;
+echo '<section id="simple_tabs" class="simple_tabs">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row justify-content-end">'.PHP_EOL;
-echo '<div class="tabbed__wrapper col-12 col-md-10">'.PHP_EOL;
+echo '<div class="simple_tabs__wrapper col-12 col-md-10">'.PHP_EOL;
 include (get_template_directory().'/views/components/tabbed/simple.php');
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
