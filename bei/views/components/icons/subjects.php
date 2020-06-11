ab7615c855ba7d3877c9258c1b7f1982186b2cb8
Ron Madriz <ron@firstcreative.com>
2020-06-11 00:19:14 -0500

    Programs - Subject

--- bei/views/components/icons/subjects.php
+++ bei/views/components/icons/subjects.php
@@ -9,7 +9,7 @@ if (have_rows('subjects')) {
 		$subject_title = get_sub_field('title');
 		$subject_desc  = get_sub_field('description');
 		echo '<div class="subjects__item col-6 col-md-3">'.PHP_EOL;
-		echo '<span class="subjects__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$subject_icon.'.svg').'</span>'.PHP_EOL;
+		echo (!empty($subject_icon)?'<span class="subjects__img">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$subject_icon.'.svg').'</span>'.PHP_EOL:'');
 		echo '<h3 class="subjects__title">'.$subject_title.'</h3>'.PHP_EOL;
 		echo '<p class="subjects__desc">'.$subject_desc.'</p>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
