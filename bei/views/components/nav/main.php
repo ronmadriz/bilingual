1184a75f8b5ea5be2ed34bf566b7182e70376497
Ron Madriz <ron@firstcreative.com>
2020-06-11 01:07:45 -0500

    logo - schedules upload

--- bei/views/components/nav/main.php
+++ bei/views/components/nav/main.php
@@ -4,7 +4,7 @@ $slogan           = get_bloginfo('description');
 $theme_location   = get_bloginfo('stylesheet_directory');
 echo '<nav class="navbar navbar-expand-lg main__nav">'.PHP_EOL;
 if (!empty($site_logo_header)) {
-	echo '<a href="'.get_bloginfo('url').'" class="navbar-brand main__nav--brand"><img src="'.esc_url($site_logo_header).'" alt="'.get_bloginfo('name').'" class="main__nav--logo img-fluid"></a>'.PHP_EOL;
+	echo '<a href="'.get_bloginfo('url').'" class="navbar-brand site__home"><img src="'.esc_url($site_logo_header).'" alt="'.get_bloginfo('name').'" class="img-fluid site__logo"></a>'.PHP_EOL;
 
 } else {
 	echo '<a href="'.get_bloginfo('url').'" class="navbar-brand">'.get_bloginfo('name').(!empty($slogan)?'<br><small>'.$slogan.'</small>':'').'</a>'.PHP_EOL;
