44f735454ca1f841869ec16c6f3e549e70b029ef
Ron Madriz <ron@firstcreative.com>
2020-06-11 11:43:28 -0500

    programs - schedule - layout

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -3,3 +3,4 @@ include (get_template_directory().'/views/global/main.php');
 include (get_template_directory().'/views/components/icons/subjects.php');
 include ('tabs.php');
 include (get_template_directory().'/views/components/schedules/table.php');
+include ('features.php');
