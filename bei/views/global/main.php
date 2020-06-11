c6592ad695e13f698e59a6add48cca56d005a825
Ron Madriz <ron@firstcreative.com>
2020-06-11 03:49:04 -0500

    logo - schedules upload

--- bei/views/global/main.php
+++ bei/views/global/main.php
@@ -5,9 +5,9 @@ if (have_posts()) {
 	echo '<div class="row">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
-		echo '<div class="col-12">'.PHP_EOL;
+		echo '<div class="main__content col-12">'.PHP_EOL;
 		echo '<h1 class="main__title">'.get_the_title().'</h1>'.PHP_EOL;
-		echo '<div class="main__content">'.get_the_content().'</div>'.PHP_EOL;
+		echo '<div class="main__desc">'.get_the_content().'</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
