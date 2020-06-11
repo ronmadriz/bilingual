30b0de5c47c7eea8615e6d75857b6694c624471f
Ron Madriz <ron@firstcreative.com>
2020-06-11 12:56:36 -0500

    programs - schedule - layout

--- bei/views/global/main.php
+++ bei/views/global/main.php
@@ -2,7 +2,6 @@
 if (have_posts()) {
 	echo '<section id="main" class="main">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
 		echo '<div class="row"><div class="main__title col-12"><h1 class="main__title--text">'.get_the_title().'</h1></div></div>'.PHP_EOL;
@@ -11,7 +10,6 @@ if (have_posts()) {
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
-	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
