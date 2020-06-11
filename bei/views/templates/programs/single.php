2b2ffc29f67608693d9ffca7642557a06f91e04a
Ron Madriz <ron@firstcreative.com>
2020-06-11 16:55:32 -0500

    page title fix

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -5,7 +5,7 @@ if (have_posts()) {
 	while (have_posts()) {
 		the_post();
 		echo '<div class="row"><div class="page__title col-12 col-md-8"><h1 class="main__title">'.get_the_title().'</h1></div></div>'.PHP_EOL;
-		echo '<div class="col-12 col-md-8 breadcrumbs__content">'.PHP_EOL;
+		echo '<div class="row"><div class="col-12 col-md-8 breadcrumbs__content">'.PHP_EOL;
 		the_breadcrumb();
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
