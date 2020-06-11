03d4cc61c0b3980a8dc823d1e5d659b74e3c168f
Ron Madriz <ron@firstcreative.com>
2020-06-11 00:59:40 -0500

    logo - schedules upload

--- bei/views/components/schedules/table.php
+++ bei/views/components/schedules/table.php
@@ -1,33 +1,34 @@
 <?php
 // Used on Program Schedules
-
-echo '<section id="schedule" class="schedule">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row">'.PHP_EOL;
-echo '<div class="col-12">'.PHP_EOL;
-echo '<table class="table">'.PHP_EOL;
-echo '<caption>CAPTION</caption>'.PHP_EOL;
-echo '<thead>'.PHP_EOL;
-echo '<tr>'.PHP_EOL;
-echo '<th>'.PHP_EOL;
-_e('Cycle # &amp; Year');
-echo '</th>'.PHP_EOL;
-echo '<th>'.PHP_EOL;
-_e('Start Date');
-echo '</th>'.PHP_EOL;
-echo '<th>'.PHP_EOL;
-_e('End Date');
-echo '</th></tr>'.PHP_EOL;
-echo '</thead>'.PHP_EOL;
-echo '<tbody>'.PHP_EOL;
-echo '<tr>'.PHP_EOL;
-echo '<td>Value 1</td>'.PHP_EOL;
-echo '<td>Value 2</td>'.PHP_EOL;
-echo '<td>Value 3</td>'.PHP_EOL;
-echo '</tr>'.PHP_EOL;
-echo '</tbody>'.PHP_EOL;
-echo '</table>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
+if (have_rows('schedules')) {
+	echo '<section id="schedule" class="schedule">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="col-12">'.PHP_EOL;
+	echo '<table class="table">'.PHP_EOL;
+	echo '<caption>CAPTION</caption>'.PHP_EOL;
+	echo '<thead>'.PHP_EOL;
+	echo '<tr>'.PHP_EOL;
+	echo '<th>'.PHP_EOL;
+	_e('Cycle # &amp; Year');
+	echo '</th>'.PHP_EOL;
+	echo '<th>'.PHP_EOL;
+	_e('Start Date');
+	echo '</th>'.PHP_EOL;
+	echo '<th>'.PHP_EOL;
+	_e('End Date');
+	echo '</th></tr>'.PHP_EOL;
+	echo '</thead>'.PHP_EOL;
+	echo '<tbody>'.PHP_EOL;
+	echo '<tr>'.PHP_EOL;
+	echo '<td>Value 1</td>'.PHP_EOL;
+	echo '<td>Value 2</td>'.PHP_EOL;
+	echo '<td>Value 3</td>'.PHP_EOL;
+	echo '</tr>'.PHP_EOL;
+	echo '</tbody>'.PHP_EOL;
+	echo '</table>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
