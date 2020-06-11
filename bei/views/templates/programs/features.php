e3a6b6cac8d9f7088d563d3cb63c33023e9bcbbf
Ron Madriz <ron@firstcreative.com>
2020-06-11 12:30:19 -0500

    programs - schedule - layout

--- bei/views/templates/programs/features.php
+++ bei/views/templates/programs/features.php
@@ -1,19 +1,19 @@
 <?php
-/*if (have_rows('features')) {
-while (have_rows('features')) {
-the_row();
-$feature_item_title   = get_sub_field('feature_item_title');
-$feature_item_content = get_sub_field('feature_item_content');
-echo '<div class="features__content col-12 col-md-6">'.PHP_EOL;
-echo '<h3 class="features__title">'.$feature_item_title.'</h3>'.PHP_EOL;
-echo '<span class="features__desc">'.$feature_item_title.'</span>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
+if (have_rows('features')) {
+	echo '<section id="featured" class="featured">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	while (have_rows('features')) {
+		the_row();
+		$featured_item_title   = get_sub_field('featured_item_title');
+		$featured_item_content = get_sub_field('featured_item_content');
+		echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
+		echo '<h3 class="featured__title">'.$feature_item_title.'</h3>'.PHP_EOL;
+		echo '<span class="featured__desc">'.$feature_item_title.'</span>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
+	wp_reset_postdata();
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
 }
-wp_reset_postdata();
-} */
-echo '<section id="features" class="features">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row">'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
