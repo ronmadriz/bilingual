d04eb1cfe2c9b7edd967b2c5601c053ddbf19d79
Ron Madriz <ron@firstcreative.com>
2020-06-11 16:28:04 -0500

    page title fix

--- bei/views/global/main.php
+++ bei/views/global/main.php
@@ -6,7 +6,7 @@ if (have_posts()) {
 		the_post();
 		echo '<div class="row"><div class="page__title col-12"><h1 class="main__title">'.get_the_title().'</h1></div></div>'.PHP_EOL;
 		the_breadcrumb();
-		echo '<div class="row"><div class="page__content col-12">'.get_the_content().'</div></div>'.PHP_EOL;
+		echo '<div class="row"><div class="page__content main__content col-12">'.get_the_content().'</div></div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
