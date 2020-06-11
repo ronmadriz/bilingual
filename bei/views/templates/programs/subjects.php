e1675f013a0a7715fbb03b5fabb0ab5fe4d54e0e
Ron Madriz <ron@firstcreative.com>
2020-06-11 12:44:51 -0500

    programs - schedule - layout

--- bei/views/templates/programs/subjects.php
+++ bei/views/templates/programs/subjects.php
@@ -2,6 +2,7 @@
 if (have_rows('subjects')) {
 	echo '<section id="subjects" class="subjects">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row"><div class="col-12"><h2>Core Subjects</h2></div></div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
 	include (get_template_directory().'/views/components/icons/subjects.php');
 	echo '</div>'.PHP_EOL;
