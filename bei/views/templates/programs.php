da17378efa861f0802c4f52bb323ea90c1e6dcd3
Ron Madriz <ron@firstcreative.com>
2020-06-11 00:17:25 -0500

    Programs - Subject

--- bei/views/templates/programs.php
+++ bei/views/templates/programs.php
@@ -1,2 +1,4 @@
 <?php
 include (get_template_directory().'/views/global/main.php');
+include (get_template_directory().'/views/components/icons/subjects.php');
+include (get_template_directory().'/views/components/icons/subjects.php');
