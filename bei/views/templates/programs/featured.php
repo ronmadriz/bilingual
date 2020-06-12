e456064ea3f33c31f429a6472cc6f1327b18dc1c
Ron Madriz <ron@firstcreative.com>
2020-06-12 16:27:07 -0500

    schedules table fix

--- bei/views/templates/programs/featured.php
+++ bei/views/templates/programs/featured.php
@@ -11,13 +11,15 @@ if (have_rows('featured')) {
 		$featured_thumb        = $featured_item_image['sizes'][$featured_size];
 
 		echo '<style type="text/css">'.PHP_EOL;
+		echo '@media(min-width:786px) {'.PHP_EOL;
 		echo '.featured__image {background-image:url('.$featured_item_image['url'].')}'.PHP_EOL;
+		echo '}'.PHP_EOL;
 		echo '</style>'.PHP_EOL;
 		echo '<div class="row">'.PHP_EOL;
 		echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
 		echo '<span class="featured__desc">'.$featured_item_content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
-		echo '<div class="featured__image col-12 col-md-6"><img src="'.esc_url($featured_thumb).'" alt="" class="img-fluid d-md-none"></div>'.PHP_EOL;
+		echo '<div class="featured__image col-12 col-md-6 text-sm-center"><img src="'.esc_url($featured_thumb).'" alt="" class="img-fluid d-md-none"></div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
