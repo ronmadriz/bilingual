a7d432a43f1873837f05789360d2f2a8d972eed6
Ron Madriz <ron@firstcreative.com>
2020-06-12 15:31:26 -0500

    schedules table fix

--- bei/views/templates/programs/featured.php
+++ bei/views/templates/programs/featured.php
@@ -8,10 +8,13 @@ if (have_rows('featured')) {
 		the_row();
 		$featured_item_title   = get_sub_field('featured_item_title');
 		$featured_item_content = get_sub_field('featured_item_content');
+		$featured_item_image   = get_sub_field('featured_item_image');
+		$featured_size         = "full";
 		echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
 		echo '<h3 class="featured__title">'.$featured_item_title.'</h3>'.PHP_EOL;
 		echo '<span class="featured__desc">'.$featured_item_content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
+		echo '<div class="featured__image col-12 col-md-6">'.wp_get_attachment_image($featured_item_image, $featured_size, "", array("class" => "img-fluid")).'</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
 	echo '</div>'.PHP_EOL;
