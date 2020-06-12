aea00f81e05eab3b369857ae7e8a85470845783a
Ron Madriz <ron@firstcreative.com>
2020-06-12 15:42:58 -0500

    schedules table fix

--- bei/views/templates/programs/featured.php
+++ bei/views/templates/programs/featured.php
@@ -3,21 +3,23 @@ if (have_rows('featured')) {
 	echo '<section id="featured" class="featured">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
 	echo '<div class="row"><div class="col-12"><h2>Program Features</h2></div></div>'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
 	while (have_rows('featured')) {
 		the_row();
 		$featured_item_title   = get_sub_field('featured_item_title');
 		$featured_item_content = get_sub_field('featured_item_content');
 		$featured_item_image   = get_sub_field('featured_item_image');
-		$featured_size         = "full";
+		echo '<style type="text/css">'.PHP_EOL;
+		echo '.featured__image {background-image:url('.$featured_item_image['url'].')}'.PHP_EOL;
+		echo '</style>'.PHP_EOL;
+		echo '<div class="row">'.PHP_EOL;
 		echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
 		echo '<h3 class="featured__title">'.$featured_item_title.'</h3>'.PHP_EOL;
 		echo '<span class="featured__desc">'.$featured_item_content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
-		echo '<div class="featured__image col-12 col-md-6">'.wp_get_attachment_image($featured_item_image, array("class" => "img-fluid")).'</div>'.PHP_EOL;
+		echo '<div class="featured__image col-12 col-md-6"><img src="'.$featured_item_image['url'].'" alt="" class="d-md-none"></div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
-	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
