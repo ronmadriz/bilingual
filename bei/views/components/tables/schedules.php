3eb72d2b134c8607918a370d6ea4271e975c6556
Ron Madriz <ron@firstcreative.com>
2020-06-17 16:17:34 -0500

    calendar and restructure

--- bei/views/components/tables/schedules.php
+++ bei/views/components/tables/schedules.php
@@ -15,32 +15,32 @@ if (have_rows('schedules_section')) {
 				echo '<div class="schedule__wrapper col-12 col-md-6">'.PHP_EOL;
 				if (!empty($schedule_table)) {
 					if (!empty($schedule_table['caption'])) {
-						echo '<h3 class="schedule__label">'.$schedule_table['caption'].'</h3>';
+						echo '<h3 class="schedule__label">'.$schedule_table['caption'].'</h3>'.PHP_EOL;
 					}
 					echo '<table class="table schedule__table'.(!empty($schedule_table['caption'])?' w_caption':'').'">'.PHP_EOL;
 					if (!empty($schedule_table['header'])) {
-						echo '<thead>';
-						echo '<tr>';
+						echo '<thead>'.PHP_EOL;
+						echo '<tr>'.PHP_EOL;
 						foreach ($schedule_table['header'] as $th) {
 							echo '<th class="schedule__headers" scope="col">';
 							echo $th['c'];
-							echo '</th>';
+							echo '</th>'.PHP_EOL;
 						}
-						echo '</tr>';
-						echo '</thead>';
+						echo '</tr>'.PHP_EOL;
+						echo '</thead>'.PHP_EOL;
 					}
-					echo '<tbody>';
+					echo '<tbody>'.PHP_EOL;
 					foreach ($schedule_table['body'] as $tr) {
-						echo '<tr class="schedule__row">';
+						echo '<tr class="schedule__row">'.PHP_EOL;
 						foreach ($tr as $key => $td) {
 							echo '<td class="schedule__data" data-label="'.$schedule_table['header'][$key]['c'].'">';
 							echo $td['c'];
-							echo '</td>';
+							echo '</td>'.PHP_EOL;
 						}
-						echo '</tr>';
+						echo '</tr>'.PHP_EOL;
 					}
-					echo '</tbody>';
-					echo '</table>';
+					echo '</tbody>'.PHP_EOL;
+					echo '</table>'.PHP_EOL;
 				}
 			}
 			wp_reset_postdata();
