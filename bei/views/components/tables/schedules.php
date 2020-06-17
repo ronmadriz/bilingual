0376735143ff6e10535e399049db9ce85cf9a72d
Ron Madriz <ron@firstcreative.com>
2020-06-17 15:48:36 -0500

    calendar and restructure

--- bei/views/components/tables/schedules.php
+++ bei/views/components/tables/schedules.php
@@ -1,60 +1,53 @@
 <?php
 // Used on Program Schedules
 if (have_rows('schedules_section')) {
+	echo '<section id="schedule" class="schedule">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
 	while (have_rows('schedules_section')) {
 		the_row();
 		$schedule_section_title = get_sub_field('schedule_section_title');
-		echo '<section id="schedule" class="schedule">'.PHP_EOL;
-		echo '<div class="container-fluid">'.PHP_EOL;
 		echo (!empty($schedule_section_title)?'<div class="row justify-content-end"><div class="col-12 schedule__title"><h2>'.$schedule_section_title.'</h2></div></div>'.PHP_EOL:'');
 		if (have_rows('schedules')) {
 			echo '<div class="row">'.PHP_EOL;
 			while (have_rows('schedules')) {
 				the_row();
-				$schedule_title = get_sub_field('schedule_title');
+				$schedule_table = get_sub_field('schedule');
 				echo '<div class="schedule__wrapper col-12 col-md-6">'.PHP_EOL;
-				echo (!empty($schedule_title)?'<h3 class="schedule__label">'.$schedule_title.'</h3>'.PHP_EOL:'');
-				echo '<table class="table schedule__table">'.PHP_EOL;
-				echo '<thead>'.PHP_EOL;
-				echo '<tr>'.PHP_EOL;
-				echo '<th class="schedule__headers">'.PHP_EOL;
-				_e('Cycle # &amp; Year', 'bei_core');
-				echo '</th>'.PHP_EOL;
-				echo '<th class="schedule__headers">'.PHP_EOL;
-				_e('Start Date', 'bei_core');
-				echo '</th>'.PHP_EOL;
-				echo '<th class="schedule__headers">'.PHP_EOL;
-				_e('End Date', 'bei_core');
-				echo '</th></tr>'.PHP_EOL;
-				echo '</thead>'.PHP_EOL;
-				if (have_rows('schedule')) {
-					echo '<tbody>'.PHP_EOL;
-					while (have_rows('schedule')) {
-						the_row();
-						$cycle_info  = get_sub_field('cycle');
-						$cycle_start = get_sub_field('start');
-						$cycle_end   = get_sub_field('end');
-						echo '<tr class="schedule__row">'.PHP_EOL;
-						echo '<td class="schedule__data">'.(!empty($cycle_info)?$cycle_info:'').'</td>'.PHP_EOL;
-						echo '<td class="schedule__data" data-label="';
-						_e('Start Date', 'bei_core');
-						echo '">'.(!empty($cycle_start)?$cycle_start:'').'</td>'.PHP_EOL;
-						echo '<td class="schedule__data" data-label="';
-						_e('End Date', 'bei_core');
-						echo '">'.(!empty($cycle_end)?$cycle_end:'').'</td>'.PHP_EOL;
-						echo '</tr>'.PHP_EOL;
+				if (!empty($schedule_table)) {
+					if (!empty($schedule_table['caption'])) {
+						echo '<h3 class="schedule__label">'.$schedule_table['caption'].'</h3>';
 					}
-					wp_reset_postdata();
-					echo '</tbody>'.PHP_EOL;
+					echo '<table class="table schedule__table'.(!empty($schedule_table['caption'])?' w_caption':'').'">'.PHP_EOL;
+					if (!empty($schedule_table['header'])) {
+						echo '<thead>';
+						echo '<tr>';
+						foreach ($schedule_table['header'] as $th) {
+							echo '<th class="schedule__headers" scope="col">';
+							echo $th['c'];
+							echo '</th>';
+						}
+						echo '</tr>';
+						echo '</thead>';
+					}
+					echo '<tbody>';
+					foreach ($schedule_table['body'] as $tr) {
+						echo '<tr class="schedule__row">';
+						foreach ($tr as $key => $td) {
+							echo '<td class="schedule__data" data-label="'.$schedule_table['header'][$key]['c'].'">';
+							echo $td['c'];
+							echo '</td>';
+						}
+						echo '</tr>';
+					}
+					echo '</tbody>';
+					echo '</table>';
 				}
-				echo '</table>'.PHP_EOL;
-				echo '</div>'.PHP_EOL;
 			}
 			wp_reset_postdata();
 			echo '</div>'.PHP_EOL;
 		}
-		echo '</div>'.PHP_EOL;
-		echo '</section>'.PHP_EOL;
 	}
 	wp_reset_postdata();
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
 }
