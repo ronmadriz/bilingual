d6a7c4f6918096c4d9b96662ceb4e2c289374d38
Ron Madriz <ron@firstcreative.com>
2020-06-17 14:08:05 -0500

    calendar and restructure

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -1,5 +1,5 @@
 <?php
-include (get_template_directory_uri().'/views/layout/content-width-thumbnail.php')
+include (get_template_directory_uri().'/views/layout/content-width-thumbnail.php');
 include ('blocks.php');
 include ('subjects.php');
 include ('schedules.php');
