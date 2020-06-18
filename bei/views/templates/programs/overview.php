ba61db5d7ea2a971057bf52015f7487e7083c977
Ron Madriz <ron@firstcreative.com>
2020-06-18 12:50:07 -0500

    calendar and restructure

--- bei/views/templates/programs/overview.php
+++ bei/views/templates/programs/overview.php
@@ -1,18 +1,18 @@
 <?php
 $program_args = array(
-	'post_type'   => 'programs',
+	'post_type'   => 'overview',
 	'post_status' => 'publish',
 );
 $program_query = new WP_Query($program_args);
 if ($program_query->have_posts()) {
-	echo '<section id="main" class="programs">'.PHP_EOL;
+	echo '<section id="overview" class="overview">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="programs__content col-12 col-md-7">'.PHP_EOL;
-	echo '<ul class="programs__list">'.PHP_EOL;
+	echo '<div class="overview__content col-12 col-md-7">'.PHP_EOL;
+	echo '<ul class="overview__list">'.PHP_EOL;
 	while ($program_query->have_posts()) {
 		$program_query->the_post();
-		echo '<li class="programs__item"><a href="'.get_the_permalink().'" class="programs__link">'.get_the_title().'</a></li>'.PHP_EOL;
+		echo '<li class="overview__item"><a href="'.get_the_permalink().'" class="overview__link">'.get_the_title().'</a></li>'.PHP_EOL;
 	}
 	wp_reset_postdata();
 	echo '</ul>'.PHP_EOL;
