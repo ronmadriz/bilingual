c40cf9dad7f9bc810766e790bdee0e958894a879
Ron Madriz <ron@firstcreative.com>
2020-06-18 18:58:01 -0500

    programs overview

--- bei/includes/cpt_files/programs_type.php
+++ bei/includes/cpt_files/programs_type.php
@@ -1,7 +1,7 @@
 <?php
 add_action('init', function () {
 		// register_extended_taxonomy( 'page_category', 'page', array() );
-		register_extended_taxonomy('programs_type', array('programs', 'programs'));
+		register_extended_taxonomy('program_type', array('programs', 'programs'));
 	});
 
 ?>
