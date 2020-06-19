0a667b659cafb2ec4da29e82b421492af62a5a97
Ron Madriz <ron@firstcreative.com>
2020-06-19 01:16:04 -0500

    programs overview - links

--- bei/views/templates/programs/overview.php
+++ bei/views/templates/programs/overview.php
@@ -7,6 +7,7 @@ if (have_posts()) {
 	echo '<div class="overview__content col-12 col-md-8">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
+		echo '<div class="overview__list">'.PHP_EOL;
 		$children = wp_list_pages(
 			array(
 				'title_li'  => '',
@@ -19,6 +20,7 @@ if (have_posts()) {
 		if ($children) {
 			echo '<ul>'.$children.'</ul>';
 		}
+		echo '</div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
 	echo '<div class="overview__sub col-12 col-md-4">'.PHP_EOL;
