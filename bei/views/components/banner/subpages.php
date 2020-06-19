18f2a265629aba8fe38b4d6dbe75e32d3f98cd0d
Ron Madriz <ron@firstcreative.com>
2020-06-19 01:48:31 -0500

    programs overview - links

--- bei/views/components/banner/subpages.php
+++ bei/views/components/banner/subpages.php
@@ -3,7 +3,7 @@ echo '<section id="subbanner" class="subbanner">'.PHP_EOL;
 echo '<div class="container-fluid subbanner__wrapper">'.PHP_EOL;
 echo '<div class="row subbanner__row align-content-center">'.PHP_EOL;
 echo '<div class="col-12 subbanner__content">'.PHP_EOL;
-echo '<h1>'.get_the_title().'</h1>'.PHP_EOL;
+echo '<h1 class="subbanner__title">'.get_the_title().'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
