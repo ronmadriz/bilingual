fdc04822eb2693fd3a0de110f01a73a67d529d7d
Ron Madriz <ron@firstcreative.com>
2020-06-18 19:36:27 -0500

    programs overview

--- bei/views/templates/programs/overview.php
+++ bei/views/templates/programs/overview.php
@@ -1,8 +1,9 @@
 <?php
+$current_post = $post->ID;
 $program_args = array(
 	'post_type'   => 'programs',
 	'post_status' => 'publish',
-	'post_parent' => $post->ID,
+	'post_parent' => $current_post,
 );
 $program_query = new WP_Query($program_args);
 if ($program_query->have_posts()) {
