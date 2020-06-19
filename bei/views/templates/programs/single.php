b39d92268edab546791f9cc5f440af8b1afe7a90
Ron Madriz <ron@firstcreative.com>
2020-06-19 11:25:09 -0500

    Programs - Tabs

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -2,6 +2,7 @@
 include (get_template_directory().'/views/layout/content-with-thumbnail.php');
 include ('single/blocks.php');
 include ('single/subjects.php');
+include ('single/tabs.php');
 include ('single/calendars.php');
 include ('single/schedules.php');
 include ('single/featured.php');
