1cfcfca4fb924c60471766651c444f00202410d4
Ron Madriz <ron@firstcreative.com>
2020-06-19 01:11:20 -0500

    programs overview - links

--- bei/includes/programs_list_walker.php
+++ bei/includes/programs_list_walker.php
@@ -2,7 +2,7 @@
 // Program Walker
 class Programs_Walker extends Walker_Nav_Menu {
 	function start_el(&$output, $item, $depth = 0, $args = [], $id = 0) {
-		$output .= "<li class='".implode(" ", $item->classes)."'>";
+		$output .= "<li class='".implode(' ', $item->classes)."'>";
 
 		if ($item->url && $item->url != '#') {
 			$output .= '<a href="'.$item->url.'">';
