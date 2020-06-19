0a5599944d36940ee52a1eef755feb152fe1b327
Ron Madriz <ron@firstcreative.com>
2020-06-19 01:42:24 -0500

    programs overview - links

--- bei/views/templates/programs/overview.php
+++ bei/views/templates/programs/overview.php
@@ -1,5 +1,5 @@
 <?php
-@include (get_template_directory().'views/components/banner/subpage.php');
+include (get_template_directory().'views/components/banner/subpage.php');
 if (have_posts()) {
 	echo '<section id="overview" class="overview">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
@@ -35,19 +35,3 @@ if (have_posts()) {
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
-/*
-$program_args = array(
-'post_type'   => 'programs',
-'post_status' => 'publish',
-);
-$program_query = new WP_Query($program_args);
-if ($program_query->have_posts()) {
-
-}
-/* echo '<ul class="overview__list">'.PHP_EOL;
-while ($program_query->have_posts()) {
-$program_query->the_post();
-echo '<li class="overview__item"><a href="'.get_the_permalink().'" class="overview__link">'.get_the_title().'</a></li>'.PHP_EOL;
-}
-wp_reset_postdata();
-echo '</ul>'.PHP_EOL; */
