54b01f4422540a4ef0e1ee41c0b9730bedcb8066
Ron Madriz <ron@firstcreative.com>
2020-06-20 01:36:48 -0500

    page default

--- bei/views/pages/default.php
+++ bei/views/pages/default.php
@@ -1,12 +1,11 @@
 <?php
-$page_content = get_the_content();
 if (have_posts()) {
 	include (get_template_directory().'/views/components/banner/subpages.php');
 	echo '<section id="main-content">'.PHP_EOL;
 	echo '<div class="container">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
-		echo '<div class="row"><div id="content" class="col">'.$page_content.'</div></div>'.PHP_EOL;
+		echo '<div class="row"><div id="content" class="col">'.get_the_content().'</div></div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
