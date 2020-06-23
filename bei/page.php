c1091494f0169b9d544a825ec4165900eff8219e
Ron Madriz <ron@firstcreative.com>
2020-06-23 16:16:55 -0500

    page layout - image with block

--- bei/page.php
+++ bei/page.php
@@ -3,4 +3,5 @@ get_header();
 include ('views/components/banner/subpages.php');
 include ('views/pages/default.php');
 include ('views/layout/testimonial.php');
+include ('views/layout/imagebar.php');
 get_footer();
