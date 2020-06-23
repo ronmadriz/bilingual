611cca27c7e055d380c81ffd44ddfe4d824173da
Ron Madriz <ron@firstcreative.com>
2020-06-23 13:52:01 -0500

    advising - box - styling

--- bei/views/pages/default.php
+++ bei/views/pages/default.php
@@ -1,7 +1,7 @@
 <?php
 if (have_posts()) {
 	include (get_template_directory().'/views/components/banner/subpages.php');
-	echo '<section id="main-content">'.PHP_EOL;
+	echo '<section id="main-content" class="main-content">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
