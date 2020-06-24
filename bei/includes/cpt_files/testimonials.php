353f4f4badd2476eaa6fb9318df5b3d02a156951
Ron Madriz <ron@firstcreative.com>
2020-06-24 09:46:54 -0500

    testimonials - creation

--- bei/includes/cpt_files/testimonials.php
+++ bei/includes/cpt_files/testimonials.php
@@ -3,7 +3,7 @@ add_action('init', function () {
 		register_extended_post_type('testimonials', array(
 				'show_in_feed' => true,
 				'has_archive'  => false,
-				'menu_icon'    => false,
+				'menu_icon'    => 'dashicons-format-status',
 			), array(
 				'singular' => 'Testimonial',
 				'plural'   => 'Testimonials',
