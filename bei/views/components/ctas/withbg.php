122d877ee5bd7f1df9c135ce5b1fec870fb57556
Ron Madriz <ron@firstcreative.com>
2020-06-24 14:09:12 -0500

    cta - with background

--- bei/views/components/ctas/withbg.php
+++ bei/views/components/ctas/withbg.php
@@ -5,11 +5,11 @@
 </style>
 <section id="cta" class="cta withBg">
 	<div class="container-fluid">
-		<div class="row ctaBg__wrapper">
-			<div class="col-12 ctaBg__content">
-				<span class="ctaBg__title"><h2 class="ctaBg__title--text">Get Started Today on Your Future!</h2></span>
-				<span class="ctaBg__details">Whether you&apos;re looking to learn a new language or begin your journey towards becoming a U.S. citizen <abbr title="Bilingual Education Institute">BEI</abbr> has you covered!</span>
-				<span class="ctaBg__button"><a href="#" class="ctaBg__button--btn button__green">Register</a></span>
+		<div class="row cta__wrapper">
+			<div class="col-12 cta__content">
+				<span class="cta__title"><h2 class="cta__title--text">Get Started Today on Your Future!</h2></span>
+				<span class="cta__details">Whether you&apos;re looking to learn a new language or begin your journey towards becoming a U.S. citizen <abbr title="Bilingual Education Institute">BEI</abbr> has you covered!</span>
+				<span class="cta__button"><a href="#" class="cta__button--btn button__green">Register</a></span>
 			</div>
 		</div>
 	</div>
