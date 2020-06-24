9851587bbb2ed28869b5183207567ea916208bdf
Ron Madriz <ron@firstcreative.com>
2020-06-24 16:03:20 -0500

    cta - with background

--- bei/views/components/tabbed/simple.php
+++ bei/views/components/tabbed/simple.php
@@ -6,7 +6,7 @@ if (have_rows('simple_tabs')) {
 		the_row();
 		$tab__item            = get_sub_field('tab__item');
 		$simple_section_title = get_sub_field('section_title');
-		echo '<div class="row"><div class="section-title col-12">'.$simple_section_title.'</div></div>'.PHP_EOL;
+		echo '<div class="row"><div class="simple_tabs__title col-12"><h2 class="simple_section_title--text">'.$simple_section_title.'</h2></div></div>'.PHP_EOL;
 		echo '<div class="row justify-content-end">'.PHP_EOL;
 		echo '<div class="simple_tabs__wrapper col-12">'.PHP_EOL;
 		if ($tab__item) {
