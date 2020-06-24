70747f201ed6ed4ac2851777bec2f29bd86950d7
Ron Madriz <ron@firstcreative.com>
2020-06-24 14:02:46 -0500

    new font

--- bei/views/components/ctas/withbg.php
+++ bei/views/components/ctas/withbg.php
@@ -8,7 +8,7 @@
 		<div class="row ctaBg__wrapper">
 			<div class="col-12 ctaBg__content">
 				<h2 class="ctaBg__title">Get Started Today on Your Future!</h2>
-				<span class="ctaBg__content">Whether you&apos;re looking to learn a new language or begin your journey towards becoming a U.S. citizen <abbr title="Bilingual Education Institute">BEI</abbr> has you covered!</span>
+				<span class="ctaBg__details">Whether you&apos;re looking to learn a new language or begin your journey towards becoming a U.S. citizen <abbr title="Bilingual Education Institute">BEI</abbr> has you covered!</span>
 				<span class="ctaBg__button"><a href="#" class="ctaBg__button--btn button__green">Register</a></span>
 			</div>
 		</div>
