a054c0f888f4b6ea7f3e81f9e53022a2817f98dd
Ron Madriz <ron@firstcreative.com>
2020-06-24 13:36:32 -0500

    new font

--- bei/page.php
+++ bei/page.php
@@ -2,6 +2,7 @@
 get_header();
 include ('views/components/banner/subpages.php');
 include ('views/pages/default.php');
-include ('views/layout/testimonial.php');
+include ('views/components/ctas/withbg.php');
 include ('views/layout/imagebar.php');
+include ('views/layout/testimonial.php');
 get_footer();
