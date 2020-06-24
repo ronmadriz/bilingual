ad5b6c15d08067d00733686ecbd9083d624bb16e
Ron Madriz <ron@firstcreative.com>
2020-06-24 15:56:48 -0500

    cta - with background

--- bei/views/components/tabbed/simple.php
+++ bei/views/components/tabbed/simple.php
@@ -1,7 +1,9 @@
 <?php
 if (have_rows('simple_tabs')) {
+	$simple_section_title = get_sub_field('section_title');
 	echo '<section id="simple_tabs" class="simple_tabs">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row"><div class="section-title col-12">'.$simple_section_title.'</div></div>'.PHP_EOL;
 	echo '<div class="row justify-content-end">'.PHP_EOL;
 	echo '<div class="simple_tabs__wrapper col-12">'.PHP_EOL;
 	while (have_rows('simple_tabs')) {
