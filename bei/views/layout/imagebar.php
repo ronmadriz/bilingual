d0223253e1cacb02de0b311ed52ab66e3f627b02
Ron Madriz <ron@firstcreative.com>
2020-06-25 14:55:45 -0500

    pagebuilder - build out

--- bei/views/layout/imagebar.php
+++ bei/views/layout/imagebar.php
@@ -1,9 +1,10 @@
 <?php
-if( have_rows( 'imagebar' ) ){
+$bei_fl_id = get_the_ID();
+if( have_rows( 'imagebar', $bei_fl_id ) ){
 	echo '<section id="imagebar" class="imagebar">'.PHP_EOL;
 	echo '<div class="container-fluid imagebar__wrapper">'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
-	 while ( have_rows( 'imagebar' ) ) {
+	 while ( have_rows( 'imagebar', $bei_fl_id ) ) {
 	 	the_row();
 	 	$ib_image = get_sub_field('image');
 	 	$ib_content = get_sub_field('content');
