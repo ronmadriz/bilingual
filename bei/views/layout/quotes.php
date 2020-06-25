4893816f3245d99863c8add376f86c373448f34b
Ron Madriz <ron@firstcreative.com>
2020-06-25 10:38:14 -0500

    quotes - stying

--- bei/views/layout/quotes.php
+++ bei/views/layout/quotes.php
@@ -1,9 +1,9 @@
 <section id="quotes" class="quotes">
 	<div class="container-fluid">
-		<div class="row majors__wrapper">
-			<div class="col-12 majors__content">
-				<span class="majors__title"><h2 class="majors__title--text">What is the TOEFL?</h2></span>
-				<span class="majors__desc">
+		<div class="row quotes__wrapper">
+			<div class="col-12 quotes__content">
+				<span class="quotes__title"><h2 class="quotes__title--text">What is the TOEFL?</h2></span>
+				<span class="quotes__desc">
 					<p>Before you can apply to a U.S. college, international students first have to meet the English language requirements for admission to demonstrate that they can use English in an academic setting.  The Test of English as a Foreign Language (TOEFL) is used by over 11,000 universities and, more importantly, it is preferred by 9 out of 10 American universities. The minimum scores vary from school to school and also between undergraduate and graduate students. It can be a very long and intensive process to prepare for the TOEFL. Thanks to BEI’s University Partnerships, we will arm you with everything you need to apply to U.S. colleges and universities without having to take the dreaded TOEFL exam.</p>
 				</span>
 			</div>
