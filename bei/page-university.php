1dc73528491e109213b6448fc78bba3cddc669a7
Ron Madriz <ron@firstcreative.com>
2020-06-25 14:52:11 -0500

    pagebuilder - build out

--- bei/page-university.php
+++ bei/page-university.php
@@ -7,6 +7,7 @@ get_header();
 include ('views/components/banner/subpages.php');
 include ('views/layout/content-with-thumbnail.php');
 include ('views/global/pagebuilder.php');
+// include ('views/layout/imagebar.php');
 // include ('views/layout/quotes.php');
 // include ('views/components/logos/logos.php');
 // include ('views/components/tables/simple_tables.php');
