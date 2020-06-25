b2ed6326339d89198df60bbfef35f96c1a8feb99
Ron Madriz <ron@firstcreative.com>
2020-06-25 16:06:59 -0500

    pagebuilder - build out

--- bei/views/global/pagebuilder.php
+++ bei/views/global/pagebuilder.php
@@ -9,7 +9,7 @@ if (have_rows('page_builder', $bei_fl_id)) {
 		} elseif (get_row_layout() == 'quotes') {
 			include (get_template_directory().'/views/layout/quotes.php');
 		} elseif (get_row_layout() == 'fl_logos') {
-			include (get_template_directory().'/views/layout/flexible-logos.php');
+			include (get_template_directory().'/views/compontents/logos/flexible-logos.php');
 		}
 	}
 }
