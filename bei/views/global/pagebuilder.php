cc041dd6523ea9ccbd146b782daf0c3974322adc
Ron Madriz <ron@firstcreative.com>
2020-06-25 14:41:09 -0500

    quotes - stying

--- bei/views/global/pagebuilder.php
+++ bei/views/global/pagebuilder.php
@@ -1,8 +1,8 @@
 <?php
 // ID of the current item in the WordPress Loop
 $bei_fl_id = get_the_ID();
-if (have_rows('page_builder'), $bei_fl_id) {
-	while (have_rows('page_builder'), $bei_fl_id) {
+if (have_rows('page_builder', $bei_fl_id) {
+	while (have_rows('page_builder', $bei_fl_id) {
 		the_row();
 		if (get_row_layout() == 'imagebar') {
 			get_template_part(get_template_directory().'views/layout/imagebar.php', 'imagebar');
