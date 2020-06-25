26f4d06423aaeebdd7c4e48aaf49a946073df2cc
Ron Madriz <ron@firstcreative.com>
2020-06-25 14:08:40 -0500

    quotes - stying

---
+++ bei/page-university.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: University Partnerships
+ *
+ */
+get_header();
+include ('views/components/banner/subpages.php');
+include ('views/pages/default.php');
+include ('views/layout/imagebar.php');
+include ('views/layout/quotes.php');
+include ('views/components/logos/logos.php');
+include ('views/components/tables/simple_tables.php');
+get_footer();
