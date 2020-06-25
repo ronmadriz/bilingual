07ae95f44d96376a647b762da77bc4932a7b41cc
Ron Madriz <ron@firstcreative.com>
2020-06-25 11:13:41 -0500

    quotes - stying

--- bei/page.php
+++ bei/page.php
@@ -3,6 +3,7 @@ get_header();
 include ('views/components/banner/subpages.php');
 include ('views/pages/default.php');
 include ('views/layout/quotes.php');
+include ('views/components/logos/logos.php');
 include ('views/layout/testimonial.php');
 include ('views/layout/imagebar.php');
 include ('views/components/ctas/withbg.php');
