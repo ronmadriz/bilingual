cb88858c28d5c1544e10756de8d2ff69ff382661
Ron Madriz <ron@firstcreative.com>
2020-06-25 14:44:40 -0500

    quotes - stying

--- bei/views/global/pagebuilder.php
+++ bei/views/global/pagebuilder.php
@@ -5,8 +5,7 @@ if (have_rows('page_builder', $bei_fl_id)) {
 	while (have_rows('page_builder', $bei_fl_id)){
 		the_row();
 		if (get_row_layout() == 'imagebar') {
-			echo $bei_fl_id;
-//			get_template_part(get_template_directory().'views/layout/imagebar.php', 'imagebar');
+			get_template_part(get_template_directory().'views/layout/imagebar.php');
 		}
 	}
 }
