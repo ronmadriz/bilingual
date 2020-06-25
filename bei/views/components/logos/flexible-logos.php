802b3032cba4de4a9ab87ac1a1f044b909e76f5a
Ron Madriz <ron@firstcreative.com>
2020-06-25 15:37:03 -0500

    pagebuilder - build out

---
+++ bei/views/components/logos/flexible-logos.php
@@ -0,0 +1,13 @@
+<?php
+	echo '<section id="logos" class="logos">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row align-content-center text-center">'.PHP_EOL;
+	$partner_logos = get_sub_field('logos');
+	if ($partner_logos) {
+		foreach ($partner_logos as $partner_logo) {
+			echo '<div class="col-6 col-md-3 logos_item"><img src="'.$partner_logo['url'].'" alt="'.$partner_logo['alt'].'" class="logos_image img-fluid"></div>'.PHP_EOL;
+		}
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
