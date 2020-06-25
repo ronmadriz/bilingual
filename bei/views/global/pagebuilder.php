748f96c90dac35a1a849c50684fca19f4bba5ec9
Ron Madriz <ron@firstcreative.com>
2020-06-25 15:30:52 -0500

    pagebuilder - build out

--- bei/views/global/pagebuilder.php
+++ bei/views/global/pagebuilder.php
@@ -7,9 +7,9 @@ if (have_rows('page_builder', $bei_fl_id)) {
 		if (get_row_layout() == 'imagebar') {
 			include (get_template_directory().'/views/layout/imagebar.php');
 		} elseif (get_row_layout() == 'quotes') {
-			include (get_template_directory().'views/layout/quotes.php');
+			include (get_template_directory().'/views/layout/quotes.php');
 		}
 	}
 }
-// include ('views/components/logos/logos.php');
-// include ('views/components/tables/simple_tables.php');
+// include (get_template_directory().'/views/components/logos/logos.php');
+// include (get_template_directory().'/views/components/tables/simple_tables.php');
