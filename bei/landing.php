8e633efeb2ada2c1e19114d536517ac4db9e4ef5
Ron Madriz <ron@firstcreative.com>
2020-06-26 16:19:12 -0500

    pagebuilder - text right

--- bei/landing.php
+++ bei/landing.php
@@ -8,4 +8,5 @@ get_header();
 include ('views/components/banner/landing.php');
 include ('views/global/pagebuilder.php');
 include ('views/layout/withsidebar.php');
+include ('views/components/galleries/rotating.php');
 get_footer();
