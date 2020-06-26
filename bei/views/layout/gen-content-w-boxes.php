eef218dee37033527067ea3b42a3ed7403d81c7c
Ron Madriz <ron@firstcreative.com>
2020-06-26 13:43:43 -0500

    content boxes

--- bei/views/layout/gen-content-w-boxes.php
+++ bei/views/layout/gen-content-w-boxes.php
@@ -4,7 +4,7 @@ $gcwb_content = get_sub_field('gcwb_content', $bei_fl_id);
 
 $gcwb_id_raw = preg_replace("/[^a-zA-Z]/", "_", $gcwb_title);
 $gcwb_id     = strtolower($gcwb_id_raw);
-echo '<section id="content" class="gencontent">'.PHP_EOL;
+echo '<section id="'.$gcwb_id.'" class="gencontent">'.PHP_EOL;
 echo '<div class="content-fluid">'.PHP_EOL;
 echo '<div class="row align-content-center gencontent__wrapper">'.PHP_EOL;
 echo '<div class="col-12 col-md-10 gencontent__content">'.PHP_EOL;
--- bei/views/layout/gen-content.php
+++ bei/views/layout/gen-content.php
@@ -4,7 +4,7 @@ $gc_content = get_sub_field('gc_content', $bei_fl_id);
 
 $gc_id_raw = preg_replace("/[^a-zA-Z]/", "_", $gc_title);
 $gc_id     = strtolower($gc_id_raw);
-echo '<section id="content" class="gencontent">'.PHP_EOL;
+echo '<section id="'.$gc_id.'" class="gencontent">'.PHP_EOL;
 echo '<div class="content-fluid">'.PHP_EOL;
 echo '<div class="row align-content-center gencontent__wrapper">'.PHP_EOL;
 echo '<div class="col-12 col-md-10 gencontent__content">'.PHP_EOL;
