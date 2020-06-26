a57c263e72aebf01c0e0f81d9394169ea702f195
Ron Madriz <ron@firstcreative.com>
2020-06-26 11:06:43 -0500

    landing - template

--- bei/views/components/banner/landing.php
+++ bei/views/components/banner/landing.php
@@ -4,12 +4,12 @@ if (has_post_thumbnail()) {
 	echo '#banner {background-image:url('.get_the_post_thumbnail_url().');}'.PHP_EOL;
 	echo '</style>'.PHP_EOL;
 }
-echo '<section id="banner" class="landing">'.PHP_EOL;
+echo '<section id="banner" class="landing_banner">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row landing__wrapper">'.PHP_EOL;
-echo '<div class="landing__content col-12">'.PHP_EOL;
-echo '<span class="landing__title"><h1 class="landing__title--text">'.get_the_title().'</h1></span>'.PHP_EOL;
-echo '<span class="landing__desc">';
+echo '<div class="row landing_banner__wrapper">'.PHP_EOL;
+echo '<div class="landing_banner__content col-12">'.PHP_EOL;
+echo '<span class="landing_banner__title"><h1 class="landing_banner__title--text">'.get_the_title().'</h1></span>'.PHP_EOL;
+echo '<span class="landing_banner__desc">';
 the_content();
 echo '</span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
