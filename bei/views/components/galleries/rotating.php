2c972854308859267072b70acfee461fccdfdc0b
Ron Madriz <ron@firstcreative.com>
2020-06-26 16:30:23 -0500

    pagebuilder - text right

--- bei/views/components/galleries/rotating.php
+++ bei/views/components/galleries/rotating.php
@@ -1,48 +1,21 @@
 <section id="rotating" class="rotating">
-	<div class="container-fluid">
+	<div class="rotating__container container-fluid">
 		<div class="rotating__wrapper row">
-			<div class="rotating__content col-12">
-				<!--Carousel Wrapper-->
-				<div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel">
-				  <!--Slides-->
-				  <div class="carousel-inner" role="listbox">
-				    <div class="carousel-item active">
-				      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg"
-				        alt="First slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg"
-				        alt="Second slide">
-				    </div>
-				    <div class="carousel-item">
-				      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"
-				        alt="Third slide">
-				    </div>
-				  </div>
-				  <!--/.Slides-->
-				  <!--Controls-->
-				  <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
-				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Previous</span>
-				  </a>
-				  <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
-				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Next</span>
-				  </a>
-				  <!--/.Controls-->
-				  <ol class="carousel-indicators">
-				    <li data-target="#carousel-thumb" data-slide-to="0" class="active">
-				      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg" width="100">
-				    </li>
-				    <li data-target="#carousel-thumb" data-slide-to="1">
-				      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg" width="100">
-				    </li>
-				    <li data-target="#carousel-thumb" data-slide-to="2">
-				      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg" width="100">
-				    </li>
-				  </ol>
+			<div id="rotating__content" class="rotating__content col-12 carousel slide carousel-fade" data-ride="carousel">
+				<div class="rotating__inner carousel-inner" role="listbox">
+					<div class="rotating__item carousel-item active"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg"	alt="" class="rotating__item--image"></div>
+					<div class="rotating__item carousel-item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg" alt="" class="rotating__item--image"></div>
+					<div class="rotating__item carousel-item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg" alt="" class="rotating__item--image"></div>
 				</div>
-				<!--/.Carousel Wrapper-->
+			  <a class="rotating__nav rotating__nav--prev carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span>
+			  </a>
+			  <a class="rotating__nav rotating__nav--next carousel-control-next" href="#carousel-thumb" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span>
+			  </a>
+			  <ol class="carousel-indicators rotating__indicators">
+			    <li data-target="#carousel-thumb" data-slide-to="0" class="rotating__indicators--item active"></li>
+			    <li data-target="#carousel-thumb" data-slide-to="1" class="rotating__indicators--item "></li>
+			    <li data-target="#carousel-thumb" data-slide-to="2" class="rotating__indicators--item "></li>
+			  </ol>
 			</div>
 		</div>
 	</div>
