90b2348cfff0b3927adcdb86784649239e6c3e3d
Ron Madriz <ron@firstcreative.com>
2020-06-26 15:21:49 -0500

    pagebuilder - text right

--- bei/views/layout/text_right.php
+++ bei/views/layout/text_right.php
@@ -1,6 +1,6 @@
 <?
-$tr_title = get_sub_field('tr_title');
-$tr_desc  = get_sub_field('tr_desc');
+$tr_title   = get_sub_field('tr_title');
+$tr_content = get_sub_field('tr_content');
 
 $tr_id_raw = preg_replace("/[^a-zA-Z]/", "_", $tr_title);
 $tr_id     = strtolower($tr_id_raw);
@@ -9,7 +9,7 @@ echo '<section id="text_right" class="text_right">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="text_right__wrapper row">'.PHP_EOL;
 echo '<span class="text_right__title col-12"><h2 class="text_right__title--text">'.$tr_title.'</h2></span>'.PHP_EOL;
-echo '<span class="text_right__desc col-12">'.$tr_desc.'</span>'.PHP_EOL;
+echo '<span class="text_right__desc col-12">'.$tr_content.'</span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
