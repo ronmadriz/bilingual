cdc223985a76cad708af64c9c0ef098b8c65e047
Ron Madriz <ron@firstcreative.com>
2020-06-26 15:43:14 -0500

    pagebuilder - text right

---
+++ bei/views/layout/withsidebar.php
@@ -0,0 +1,7 @@
+<section class="wsb">
+	<div class="wsb__wrapper">
+		<article class="wsb__article"></article>
+		<article class="wsb__article"></article>
+		<aside class="wsb__aside"></aside>
+	</div>
+</section>
