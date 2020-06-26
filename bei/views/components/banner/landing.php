719b515fd8bec790d161cd25f4b0fa7f120e55fb
Ron Madriz <ron@firstcreative.com>
2020-06-26 10:44:43 -0500

    landing - template

--- bei/views/components/banner/landing.php
+++ bei/views/components/banner/landing.php
@@ -5,7 +5,7 @@
 	<div class="container-fluid">
 		<div class="row landing__wrapper">
 			<div class="landing__content col-12">
-				<h1 class="landing__title">Student Life</h1>
+				<span class="landing__title"><h1 class="landing__title--text">Student Life</h1></span>
 				<span class="landing__desc">
 					At BEI, it’s far from all work and no play.  With the vibrant city of Houston as our backdrop, our students are treated to a cultural playground right here in our backyard. There is no shortage of things to see and do, and here you will experience things you never dreamed possible. As you enjoy the best that Houston has to offer, you will benefit from a top-tier education at one of the country’s most respected schools.  At BEI, we open the door to a world of opportunity.  Join us and begin living the life you have always wanted.
 				</span>
