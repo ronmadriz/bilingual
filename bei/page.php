a2d436872fceaa8eaca3b169835365c31ca95833
Ron Madriz <ron@firstcreative.com>
2020-06-26 15:46:35 -0500

    pagebuilder - text right

--- bei/page.php
+++ bei/page.php
@@ -3,5 +3,4 @@ get_header();
 include ('views/components/banner/subpages.php');
 include ('views/layout/content-with-thumbnail.php');
 include ('views/global/pagebuilder.php');
-include ('views/layout/withsidebar.php')
 get_footer();
