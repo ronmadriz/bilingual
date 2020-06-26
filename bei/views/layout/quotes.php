926da8e4b441df206b64fd7bbd64ee4cd72d4ffd
Ron Madriz <ron@firstcreative.com>
2020-06-26 00:24:36 -0500

    pagebuilder - build out - logos

--- bei/views/layout/quotes.php
+++ bei/views/layout/quotes.php
@@ -1,25 +1,25 @@
 <?php
- 	$qt_title = get_sub_field('qt_title', $bei_fl_id);
- 	$qt_boxes = get_sub_field('qt_boxes', $bei_fl_id);
- 	$qt_content = get_sub_field('qt_content', $bei_fl_id);
-	echo '<section id="quotes" class="quotes">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row quotes__wrapper">'.PHP_EOL;
-	echo '<div class="col-12 quotes__content">'.PHP_EOL;
-	echo '<span class="quotes__title"><h2 class="quotes__title--text">'.$qt_title.'</h2></span>'.PHP_EOL;
-	if(have_rows('qt_boxes')){
-		echo '<ul class="quotes__boxes list-unstyled list-inline">'.PHP_EOL;
-		while(have_rows('qt_boxes')){
-			the_row();
-			$qt_box_text = get_sub_field('qt_box_text');
-			echo '<li class="quotes__boxes--item list-inline-item">'.$qt_box_text.'</li>'.PHP_EOL;
-		}
-		echo '</ul>'.PHP_EOL;
+$qt_title   = get_sub_field('qt_title', $bei_fl_id);
+$qt_boxes   = get_sub_field('qt_boxes', $bei_fl_id);
+$qt_content = get_sub_field('qt_content', $bei_fl_id);
+echo '<section id="quotes" class="quotes">'.PHP_EOL;
+echo '<div class="container-fluid">'.PHP_EOL;
+echo '<div class="row quotes__wrapper">'.PHP_EOL;
+echo '<div class="col-12 quotes__content">'.PHP_EOL;
+echo '<span class="section__title"><h2 class="section__title--text">'.$qt_title.'</h2></span>'.PHP_EOL;
+if (have_rows('qt_boxes')) {
+	echo '<ul class="quotes__boxes list-unstyled list-inline">'.PHP_EOL;
+	while (have_rows('qt_boxes')) {
+		the_row();
+		$qt_box_text = get_sub_field('qt_box_text');
+		echo '<li class="quotes__boxes--item list-inline-item">'.$qt_box_text.'</li>'.PHP_EOL;
 	}
-	echo '<span class="quotes__desc">'.PHP_EOL;
-	echo $qt_content;
-	echo '</span>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
+	echo '</ul>'.PHP_EOL;
+}
+echo '<span class="quotes__desc">'.PHP_EOL;
+echo $qt_content;
+echo '</span>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
