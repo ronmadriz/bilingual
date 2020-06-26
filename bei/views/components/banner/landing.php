885269bc1e54a19c2ac7c1577722225bde4675cf
Ron Madriz <ron@firstcreative.com>
2020-06-26 11:11:12 -0500

    landing - template

--- bei/views/components/banner/landing.php
+++ bei/views/components/banner/landing.php
@@ -1,16 +1,21 @@
 <?php
-if (has_post_thumbnail()) {
-	echo '<style type="text/css">'.PHP_EOL;
-	echo '#banner {background-image:url('.get_the_post_thumbnail_url().');}'.PHP_EOL;
-	echo '</style>'.PHP_EOL;
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+		if (has_post_thumbnail()) {
+			echo '<style type="text/css">'.PHP_EOL;
+			echo '#banner {background-image:url('.get_the_post_thumbnail_url().');}'.PHP_EOL;
+			echo '</style>'.PHP_EOL;
+		}
+		echo '<section id="banner" class="landing_banner">'.PHP_EOL;
+		echo '<div class="container-fluid">'.PHP_EOL;
+		echo '<div class="row landing_banner__wrapper">'.PHP_EOL;
+		echo '<div class="landing_banner__content col-12">'.PHP_EOL;
+		echo '<span class="landing_banner__title"><h1 class="landing_banner__title--text">'.get_the_title().'</h1></span>'.PHP_EOL;
+		echo '<span class="landing_banner__desc">'.get_the_content().'</span>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</section>'.PHP_EOL;
+	}
 }
-echo '<section id="banner" class="landing_banner">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row landing_banner__wrapper">'.PHP_EOL;
-echo '<div class="landing_banner__content col-12">'.PHP_EOL;
-echo '<span class="landing_banner__title"><h1 class="landing_banner__title--text">'.get_the_title().'</h1></span>'.PHP_EOL;
-echo '<span class="landing_banner__desc">'.get_the_content().'</span>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
