f4dec0375ba1c05075773bfeb2c1b4b250d8d140
Ron Madriz <ron@firstcreative.com>
2020-06-26 14:32:04 -0500

    content boxes - svg

--- bei/views/layout/tiled.php
+++ bei/views/layout/tiled.php
@@ -1,22 +1,28 @@
 <?php
 $ti_section_title = get_sub_field('ti_section_title');
-echo '<section id="tiled" class="tiled">'.PHP_EOL;
+
+$ti_id_raw = preg_replace("/[^a-zA-Z]/", "_", $ti_title);
+$ti_id     = strtolower($ti_id_raw);
+
+echo '<section id="'.$ti_id.'" class="tiled">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo (!empty($ti_section_title)?'<div class="row"><span class="section__title col-12"><h2 class="section__title--text">'.$ti_section_title.'</h2></span></div>'.PHP_EOL:'');
 if (have_rows('tiles')) {
 	echo '<div class="tiled__wrapper row">'.PHP_EOL;
 	while (have_rows('tiles')) {
 		the_row();
-		$ti_title     = get_sub_field('ti_title');
-		$ti_image     = get_sub_field('ti_image');
+		$ti_title   = get_sub_field('ti_title');
+		$ti_image   = get_sub_field('ti_image');
+		$ti_content = get_sub_field('ti_content');
+		$ti_link    = get_sub_field('ti_link');
+
 		$ti_img_size  = 'medium';
 		$ti_link_size = 'large';
 		$ti_thumb     = $ti_image['sizes'][$ti_img_size];
 		$ti_link_img  = $ti_image['sizes'][$ti_link_size];
-		$ti_content   = get_sub_field('ti_content');
-		$ti_link      = get_sub_field('ti_link');
+
 		echo '<figure class="tiled__item card col-12 col-md-3">'.PHP_EOL;
-		echo '<a href="'.$ti_link_img.'" class="tiled__image--link" data-toggle="lightbox"><img src="'.esc_url($ti_thumb).'" alt="'.$ti_image['alt'].'" class="tiled__image card-img-top img-fluid"></a>'.PHP_EOL;
+		echo '<a href="'.$ti_link_img.'" class="tiled__image--link" data-toggle="lightbox" data-gallery="'.$ti_id.'-gallery"><img src="'.esc_url($ti_thumb).'" alt="'.$ti_image['alt'].'" class="tiled__image card-img-top img-fluid"></a>'.PHP_EOL;
 		echo '<figcaption class="tiled__content">'.PHP_EOL;
 		echo (!empty($ti_title)?'<span class="tiled__title"><h3 class="tiled__title--text card-title">'.$ti_title.'</h3></span>'.PHP_EOL:'');
 		echo (!empty($ti_content)?'<span class="tiled__desc card-text">'.$ti_content.'</span>'.PHP_EOL:'');
