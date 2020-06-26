47543d0c2e95dbfb19776635773836b80c13c503
Ron Madriz <ron@firstcreative.com>
2020-06-26 09:59:51 -0500

    landing - template

---
+++ bei/landing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Landing Page
+ *
+ */
+
+get_header();
+include ('views/components/banner/landing.php');
+include ('views/pages/default.php');
+include ('views/layout/infoblocks.php');
+get_footer();
