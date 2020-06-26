b9214bf92d1651a07cae4aa16024c053d631149b
Ron Madriz <ron@firstcreative.com>
2020-06-26 02:29:28 -0500

    pagebuilder - build out - tiled

--- bei/views/layout/tiled.php
+++ bei/views/layout/tiled.php
@@ -9,10 +9,12 @@ if (have_rows('tiles')) {
 		the_row();
 		$ti_title   = get_sub_field('ti_title');
 		$ti_image   = get_sub_field('ti_image');
+		$ti_size    = 'large';
+		$ti_thumb   = $ti_image['sizes'][$ti_size];
 		$ti_content = get_sub_field('ti_content');
 		$ti_link    = get_sub_field('ti_link');
 		echo '<figure class="tiled__item card col-12 col-md-3">'.PHP_EOL;
-		echo '<img src="'.$ti_image['url'].'" alt="'.$ti_image['alt'].'" class="tiled__image card-img-top img-fluid">'.PHP_EOL;
+		echo '<img src="'.esc_url($ti_thumb).'" alt="'.$ti_image['alt'].'" class="tiled__image card-img-top img-fluid">'.PHP_EOL;
 		echo '<figcaption class="tiled__content">'.PHP_EOL;
 		echo (!empty($ti_title)?'<span class="tiled__title"><h3 class="tiled__title--text card-title">'.$ti_title.'</h3></span>'.PHP_EOL:'');
 		echo (!empty($ti_content)?'<span class="tiled__desc card-text">'.$ti_content.'</span>'.PHP_EOL:'');
