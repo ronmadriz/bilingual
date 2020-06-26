aa976dd45d526978a56647d3d02c3827e36301c0
Ron Madriz <ron@firstcreative.com>
2020-06-26 15:58:29 -0500

    pagebuilder - text right

--- bei/views/layout/withsidebar.php
+++ bei/views/layout/withsidebar.php
@@ -1,7 +1,7 @@
 <section class="wsb">
 	<div class="wsb__wrapper">
-		<article class="wsb__article"></article>
-		<article class="wsb__article"></article>
-		<aside class="wsb__aside"></aside>
+		<div class="wsb__first"></div>
+		<div class="wsb__second"></div>
+		<aside class="wsb__third"></aside>
 	</div>
 </section>
