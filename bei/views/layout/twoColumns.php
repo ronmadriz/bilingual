45bac4af024ae13ec52713c218256947bbaf9051
Ron Madriz <ron@firstcreative.com>
2020-06-27 17:46:16 -0500

    two Column Layout - main column setup

--- bei/views/layout/twoColumns.php
+++ bei/views/layout/twoColumns.php
@@ -10,14 +10,25 @@ echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row twoColumns__wrapper">'.PHP_EOL;
 echo '<div class="twoColumns__main col-12 col-md-7'.(($column_alignment == true)?' order-12':'').'">'.PHP_EOL;
 echo '<div class="twoColumns__main--content">'.$main_column_content.'</div>'.PHP_EOL;
+if (have_rows('main_column_flip')) {
+	while (have_rows('main_column_flip')) {
+		the_row();
+		echo '<article></article>';
+	}
+}
 echo '</div>'.PHP_EOL;
-echo '<aside class="twoColumns__sidebar col-12 col-md-5">'.PHP_EOL;
-echo '</aside>'.PHP_EOL;
+
+if (have_rows('side_column_articles')) {
+	echo '<aside class="twoColumns__sidebar col-12 col-md-5">'.PHP_EOL;
+	while (have_rows('side_column_articles')) {
+		the_row();
+		echo '<div></div>';
+	}
+	echo '</aside>'.PHP_EOL;
+}
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
 
 /*
-$main_column_flip
 $side_column_image
-$side_column_articles
  */
