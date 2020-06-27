10c5164eace163e9d46149e339dd44bd5a986b07
Ron Madriz <ron@firstcreative.com>
2020-06-27 16:24:57 -0500

    slider fix

--- bei/views/components/galleries/rotating.php
+++ bei/views/components/galleries/rotating.php
@@ -8,7 +8,7 @@ if ($ri_images) {
 	echo '<div class="rotating__inner carousel-inner" role="listbox">'.PHP_EOL;
 	$ri_counter = 0;
 	foreach ($ri_images as $ri_image) {
-		echo '<div class="rotating__item carousel-item"><img class="d-block w-100" src="'.esc_url($ri_image['url']).'" alt="" class="rotating__item--image'.(($ri_counter == 0)?' active':'').'"></div>'.PHP_EOL;
+		echo '<div class="rotating__item carousel-item"><img class="d-block w-100" src="'.esc_url($ri_image['sizes']['large']).'" alt="" class="rotating__item--image'.(($ri_counter == 0)?' active':'').'"></div>'.PHP_EOL;
 		$ri_counter++;
 	}
 	echo '</div>'.PHP_EOL;
