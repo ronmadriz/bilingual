8a09535841e3546f7a4bf775d3d3f421ad10c636
Ron Madriz <ron@firstcreative.com>
2020-06-27 17:10:03 -0500

    sprites

--- bei/views/layout/withsidebar.php
+++ bei/views/layout/withsidebar.php
@@ -1,14 +1,23 @@
 <?php
-$column_alignment     = get_sub_field();// True / False
-$main_column_content  = get_sub_field();// Wysiwyg Editor
-$main_column_flip     = get_sub_field();// Repeater
-$side_column_image    = get_sub_field();// Image
-$side_column_articles = get_sub_field();// Repeater
+$column_alignment     = get_sub_field('column_alignment');// True / False
+$main_column_content  = get_sub_field('main_column_content');// Wysiwyg Editor
+$main_column_flip     = get_sub_field('main_column_flip');// Repeater
+$side_column_image    = get_sub_field('side_column_image');// Image
+$side_column_articles = get_sub_field('side_column_articles');// Repeater
 
 echo '<section class="wsb">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row wsb__wrapper">'.PHP_EOL;
-echo '<div class="col-12 col-md-6">'.PHP_EOL;
+echo '<div class="wsb__main col-12 col-md-7'.(($column_alignment == true)?' order-12':'').'">'.PHP_EOL;
+echo '<div class="wsb__main--content">'.$main_column_content.'</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
+echo '<aside class="wsb__sidebar col-12 col-md-5">'.PHP_EOL;
+echo '</aside>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
+
+/*
+$main_column_flip
+$side_column_image
+$side_column_articles
+ */
