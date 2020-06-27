d5e23b5ddd1082cca0a617583493cbcc900fd7aa
Ron Madriz <ron@firstcreative.com>
2020-06-27 17:25:34 -0500

    two Column Layout - main column setup

--- bei/landing.php
+++ bei/landing.php
@@ -7,6 +7,4 @@
 get_header();
 include ('views/components/banner/landing.php');
 include ('views/global/pagebuilder.php');
-// include ('views/layout/withsidebar.php');
-// include ('views/components/galleries/rotating.php');
 get_footer();
--- bei/views/layout/withsidebar.php
+++ bei/views/layout/withsidebar.php
@@ -5,13 +5,13 @@ $main_column_flip     = get_sub_field('main_column_flip');// Repeater
 $side_column_image    = get_sub_field('side_column_image');// Image
 $side_column_articles = get_sub_field('side_column_articles');// Repeater
 
-echo '<section class="wsb">'.PHP_EOL;
+echo '<section id="twoColumns" class="twoColumns">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row wsb__wrapper">'.PHP_EOL;
-echo '<div class="wsb__main col-12 col-md-7'.(($column_alignment == true)?' order-12':'').'">'.PHP_EOL;
-echo '<div class="wsb__main--content">'.$main_column_content.'</div>'.PHP_EOL;
+echo '<div class="row twoColumns__wrapper">'.PHP_EOL;
+echo '<div class="twoColumns__main col-12 col-md-7'.(($column_alignment == true)?' order-12':'').'">'.PHP_EOL;
+echo '<div class="twoColumns__main--content">'.$main_column_content.'</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-echo '<aside class="wsb__sidebar col-12 col-md-5">'.PHP_EOL;
+echo '<aside class="twoColumns__sidebar col-12 col-md-5">'.PHP_EOL;
 echo '</aside>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
