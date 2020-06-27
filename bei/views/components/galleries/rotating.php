fbe1f396c3374d4276d09e7e9582a94b8e333711
Ron Madriz <ron@firstcreative.com>
2020-06-27 15:27:13 -0500

    slider fix

--- bei/views/components/galleries/rotating.php
+++ bei/views/components/galleries/rotating.php
@@ -2,7 +2,8 @@
 echo '<section id="rotating" class="rotating">'.PHP_EOL;
 echo '<div class="rotating__container container-fluid">'.PHP_EOL;
 echo '<div class="rotating__wrapper row">'.PHP_EOL;
-$ri_images = get_sub_field('ri_images');
+$ri_images     = get_sub_field('ri_images');
+$ri_images_ids = array();
 if ($ri_images) {
 	echo '<div id="rotating__content" class="rotating__content col-12 carousel slide carousel-fade" data-ride="carousel">'.PHP_EOL;
 	echo '<div class="rotating__inner carousel-inner" role="listbox">'.PHP_EOL;
