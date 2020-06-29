85815c958a0f25ffd0f5cb4612960e055f0858f9
Ron Madriz <ron@firstcreative.com>
2020-06-29 15:46:40 -0500

    Accordion - component

--- bei/views/components/accordion/index.php
+++ bei/views/components/accordion/index.php
@@ -21,7 +21,7 @@ if (have_rows('acc_accordion_item')) {
 		echo '<div class="card-header accordion__header" id="'.$acc_item_id.'-'.$acc_counter.'">'.PHP_EOL;
 		echo '<button class="btn btn-link" data-toggle="collapse" data-target="#'.$acc_item_id.'_content-'.$acc_counter.'" aria-expanded="'.(($acc_counter == 0?'true':'false')).'" aria-controls="'.$acc_item_id.'_content-'.$acc_counter.'">'.$acc_item_title.'</button>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
-		echo '<div id="'.$acc_item_id.'_content-'.$acc_counter.'" class="collapse '.(($acc_counter == 0?'show':'')).'  accordion__content" aria-labelledby="'.$acc_item_id.'" data-parent="#'.$acc_id.'-accordion">'.PHP_EOL;
+		echo '<div id="'.$acc_item_id.'_content-'.$acc_counter.'" class="collapse '.(($acc_counter == 0?'show':'')).'  accordion__content" aria-labelledby="'.$acc_item_id.'-'.$acc_counter.'" data-parent="#'.$acc_id.'-accordion">'.PHP_EOL;
 		echo '<div class="card-body accordion__desc">'.PHP_EOL;
 		echo $acc_item_content;
 		echo '</div>'.PHP_EOL;
