0f6fdc19d1aaad175fd5b9eb10b34b472e6b52f1
Ron Madriz <ron@firstcreative.com>
2020-06-30 11:00:26 -0500

    according button

--- bei/views/layout/two-item-feature.php
+++ bei/views/layout/two-item-feature.php
@@ -10,7 +10,7 @@ if (have_rows('first_item')) {
 		$tif_1__title   = get_sub_field('tif_1_title');
 		$tif_1__content = get_sub_field('tif_1_content');
 		echo '<div class="twoItemFeature__item">'.PHP_EOL;
-		echo '<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_1__icon.'.svg').'</span>'.PHP_EOL;
+		echo (!empty($tif_1__icon)?'<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_1__icon.'.svg').'</span>'.PHP_EOL:'');
 		echo '<h2 class="twoItemFeature__title">'.$tif_1__title.'</h2>'.PHP_EOL;
 		echo '<span class="twoItemFeature__content">'.$tif_1__content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
@@ -29,9 +29,9 @@ if (have_rows('second_item')) {
 		$tif_2__title   = get_sub_field('tif_2_title');
 		$tif_2__content = get_sub_field('tif_2_content');
 		echo '<div class="twoItemFeature__item">'.PHP_EOL;
-		echo '<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_2_icon.'.svg').'</span>'.PHP_EOL;
-		echo '<h2 class="twoItemFeature__title">'.$tif_2_title.'</h2>'.PHP_EOL;
-		echo '<span class="twoItemFeature__content">'.$tif_2_content.'</span>'.PHP_EOL;
+		echo (!empty($tif_2__icon)?'<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_2__icon.'.svg').'</span>'.PHP_EOL:'');
+		echo '<h2 class="twoItemFeature__title">'.$tif_2__title.'</h2>'.PHP_EOL;
+		echo '<span class="twoItemFeature__content">'.$tif_2__content.'</span>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
