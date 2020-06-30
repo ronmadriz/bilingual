38ffd26fb124c34f14fb629cf7f19cfb75125f52
Ron Madriz <ron@firstcreative.com>
2020-06-30 16:12:19 -0500

    navigation fix - f1

--- bei/functions.php
+++ bei/functions.php
@@ -347,40 +347,37 @@ add_filter('wpcf7_form_elements', function ($content) {
 
 		return $content;
 	});
+function childpages_shortcode_callback($atts) {
+	$atts = shortcode_atts(array(
+			'parent' => false,
+		), $atts, 'childpages');
 
+	$parent_id = false;
+	if ($atts['parent']) {
+		$parent = get_page_by_path($atts['parent']);
+		if ($parent) {
+			$parent_id = $parent->ID;
+		}
+	} else {// if no parent passed, then show children of current page
+		$parent_id = get_the_ID();
+	}
 
-function childpages_shortcode_callback( $atts ) {
-    $atts = shortcode_atts( array(
-        'parent' => false,
-    ), $atts, 'childpages' );
+	$result = '';
+	if (!$parent_id) {// don't waste time getting pages, if we couldn't get parent page
+		return $result;
+	}
 
-    $parent_id = false;
-    if ( $atts['parent'] ) {
-        $parent = get_page_by_path( $atts['parent'] ); 
-        if ( $parent ) {
-            $parent_id = $parent->ID;
-        }
-    } else { // if no parent passed, then show children of current page
-        $parent_id = get_the_ID();
-    }
+	$childpages = wp_list_pages(array(
+			'sort_column' => 'menu_order',
+			'title_li'    => '',
+			'child_of'    => $parent_id,
+			'echo'        => 0,
+		));
 
-    $result = '';
-    if ( ! $parent_id ) {  // don't waste time getting pages, if we couldn't get parent page
-         return $result;
-    }
+	if ($childpages) {
+		$result = '<ul>'.$childpages.'</ul>';
+	}
 
-    $childpages = wp_list_pages( array(
-        'sort_column' => 'menu_order',
-        'title_li' => '',
-        'child_of' => $parent_id,
-        'echo' => 0
-    ) );
-
-    if ( $childpages ) {
-        $result = '<ul>' . $childpages . '</ul>';
-    }
-
-    return $result;
+	return $result;
 }
-add_shortcode( 'childpages', 'childpages_shortcode_callback' );
-}
+add_shortcode('childpages', 'childpages_shortcode_callback');
