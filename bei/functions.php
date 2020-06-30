6304928c4e9110158bf3caf79b74529bb4b44797
Ron Madriz <ron@firstcreative.com>
2020-06-30 16:24:17 -0500

    navigation fix - f1

--- bei/functions.php
+++ bei/functions.php
@@ -45,6 +45,8 @@ include (THEME_INCLUDE.'/options/default.php');
 include (THEME_INCLUDE.'/breadcrumbs-functions.php');
 include (THEME_INCLUDE.'/programs_list_walker.php');
 include (THEME_INCLUDE.'/toplevel.php');
+include (THEME_INCLUDE.'/shortcodes/pages.php');
+include (THEME_INCLUDE.'/shortcodes/posts.php');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'feed_links', 2);
@@ -347,37 +349,3 @@ add_filter('wpcf7_form_elements', function ($content) {
 
 		return $content;
 	});
-function childpages_shortcode_callback($atts) {
-	$atts = shortcode_atts(array(
-			'parent' => false,
-		), $atts, 'childpages');
-
-	$parent_id = false;
-	if ($atts['parent']) {
-		$parent = get_page_by_path($atts['parent']);
-		if ($parent) {
-			$parent_id = $parent->ID;
-		}
-	} else {// if no parent passed, then show children of current page
-		$parent_id = get_the_ID();
-	}
-
-	$result = '';
-	if (!$parent_id) {// don't waste time getting pages, if we couldn't get parent page
-		return $result;
-	}
-
-	$childpages = wp_list_pages(array(
-			'sort_column' => 'menu_order',
-			'title_li'    => '',
-			'child_of'    => $parent_id,
-			'echo'        => 0,
-		));
-
-	if ($childpages) {
-		$result = '<ul>'.$childpages.'</ul>';
-	}
-
-	return $result;
-}
-add_shortcode('childpages', 'childpages_shortcode_callback');
