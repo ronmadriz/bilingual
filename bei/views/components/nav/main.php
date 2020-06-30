0dbd8758f33350a33643fece6f74c86376dc65d9
Ron Madriz <ron@firstcreative.com>
2020-06-30 13:20:51 -0500

    according button

--- bei/views/components/nav/main.php
+++ bei/views/components/nav/main.php
@@ -6,55 +6,13 @@ $theme_location   = get_bloginfo('stylesheet_directory');
 $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 
-$top_cat_pg = 'home';// Default.
-global $top_cat_pg;
-
-// Get class name from top-level Category or Page.
-global $wp_query;
-if (is_single()) {
-	$wp_query->post = $wp_query->posts[0];
-	setup_postdata($wp_query->post);
-	/* Climb Posts category hierarchy, successively replacing
-	class name top_cat_pg with slug of higher level cat. */
-	foreach ((array) get_the_category() as $cat) {
-		if (!empty($cat->slug)) {
-			$top_cat_pg = sanitize_html_class($cat->slug, $cat->cat_ID);
-			while ($cat->parent) {
-				$cat = &get_category((int) $cat->parent);
-				if (!empty($cat->slug)) {
-					$top_cat_pg = sanitize_html_class($cat->slug, $cat->cat_ID);
-				}
-			}
-		}
-	}
-} elseif (is_archive()) {
-	if (is_category()) {
-		$cat        = $wp_query->get_queried_object();
-		$top_cat_pg = $cat->slug;
-		/* Climb Category hierarchy, successively replacing
-		class name with slug of higher level cat. */
-		while ($cat->parent) {
-			$cat = &get_category((int) $cat->parent);
-			if (!empty($cat->slug)) {
-				$top_cat_pg = sanitize_html_class($cat->slug, $cat->cat_ID);
-			}
-		}
-	}
-} elseif (is_page()) {
-	global $post;
-	if ($post->post_parent) {
-		$ancestors  = get_post_ancestors($post->ID);
-		$root       = count($ancestors)-1;
-		$top_id     = $ancestors[$root];
-		$top_pg     = get_page($top_id);
-		$top_cat_pg = $top_pg->post_name;
-	} else {
-		$top_cat_pg = $post->post_name;
-	}
+if ($post->post_parent) {
+	$ancestors = get_post_ancestors($post->ID);
+	$root      = count($ancestors)-1;
+	$parent    = $ancestors[$root];
+} else {
+	$parent = $post->ID;
 }
-$parent_page[] = $top_cat_pg;
-
-return $parent_page;
 
 echo '<nav class="navbar navbar-expand-lg mainNav">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
@@ -70,7 +28,7 @@ echo '<span class="mainNav__toggler--item"></span>'.PHP_EOL;
 echo '<span class="mainNav__toggler--item"></span>'.PHP_EOL;
 echo '</button>'.PHP_EOL;
 echo '<div class="navbar-collapse collapse order-12 mainNav__container" id="navigation">'.PHP_EOL;
-if ($parent_page == 'english-for-refugees') {
+if ($post->post_parent == 313) {
 	$main_menu_args = array(
 		'theme_location'  => '',
 		'menu'            => 'refugee',
