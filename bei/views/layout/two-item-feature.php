c1555a1b43325a51a267e64239239f1b748ed062
Ron Madriz <ron@firstcreative.com>
2020-06-30 11:03:01 -0500

    according button

--- bei/views/layout/two-item-feature.php
+++ bei/views/layout/two-item-feature.php
@@ -8,7 +8,7 @@ if (have_rows('first_item')) {
 		$tif_1__icon    = get_sub_field('tif_1_icon');
 		$tif_1__title   = get_sub_field('tif_1_title');
 		$tif_1__content = get_sub_field('tif_1_content');
-		echo '<div class="twoItemFeature__item col-12 col-6">'.PHP_EOL;
+		echo '<div class="twoItemFeature__item col-12 col-md-6">'.PHP_EOL;
 		echo (!empty($tif_1__icon)?'<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_1__icon.'.svg').'</span>'.PHP_EOL:'');
 		echo '<h2 class="twoItemFeature__title">'.$tif_1__title.'</h2>'.PHP_EOL;
 		echo '<span class="twoItemFeature__content">'.$tif_1__content.'</span>'.PHP_EOL;
@@ -22,7 +22,7 @@ if (have_rows('second_item')) {
 		$tif_2__icon    = get_sub_field('tif_2_icon');
 		$tif_2__title   = get_sub_field('tif_2_title');
 		$tif_2__content = get_sub_field('tif_2_content');
-		echo '<div class="twoItemFeature__item col-12 col-6">'.PHP_EOL;
+		echo '<div class="twoItemFeature__item col-12 col-md-6">'.PHP_EOL;
 		echo (!empty($tif_2__icon)?'<span class="twoItemFeature__icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/'.$tif_2__icon.'.svg').'</span>'.PHP_EOL:'');
 		echo '<h2 class="twoItemFeature__title">'.$tif_2__title.'</h2>'.PHP_EOL;
 		echo '<span class="twoItemFeature__content">'.$tif_2__content.'</span>'.PHP_EOL;
