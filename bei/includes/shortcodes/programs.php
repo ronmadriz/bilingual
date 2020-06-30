071e40560debe2ac02af10fd664612565b5c505f
Ron Madriz <ron@firstcreative.com>
2020-06-30 16:28:37 -0500

    navigation fix - f1

---
+++ bei/includes/shortcodes/programs.php
@@ -0,0 +1,36 @@
+<?php
+function childprograms_shortcode_callback($atts) {
+	$atts = shortcode_atts(array(
+			'parent' => false,
+		), $atts, 'childprograms');
+
+	$parent_id = false;
+	if ($atts['parent']) {
+		$parent = get_page_by_path($atts['parent']);
+		if ($parent) {
+			$parent_id = $parent->ID;
+		}
+	} else {// if no parent passed, then show children of current page
+		$parent_id = get_the_ID();
+	}
+
+	$postresult = '';
+	if (!$parent_id) {// don't waste time getting pages, if we couldn't get parent page
+		return $postresult;
+	}
+
+	$childprograms = wp_list_pages(array(
+			'post_type'   => 'post',
+			'sort_column' => 'menu_order',
+			'title_li'    => '',
+			'child_of'    => $parent_id,
+			'echo'        => 0,
+		));
+
+	if ($childprograms) {
+		$postresult = '<ul>'.$childprograms.'</ul>';
+	}
+
+	return $postresult;
+}
+add_shortcode('childprograms', 'childprograms_shortcode_callback');
