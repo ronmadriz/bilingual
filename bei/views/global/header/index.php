03a7d852e2873c095af3d649fc6ab1e77d3ac852
Ron Madriz <ron@firstcreative.com>
2020-06-30 12:04:22 -0500

    according button

--- bei/views/global/header/index.php
+++ bei/views/global/header/index.php
@@ -7,7 +7,15 @@ $theme_location    = get_template_directory();
 
 include ($theme_location.'/views/components/nav/subsites.php');
 
-if (is_child(313)) {
+if ($post->post_parent) {
+	$ancestors = get_post_ancestors($post->ID);
+	$root      = count($ancestors)-1;
+	$parent    = $ancestors[$root];
+} else {
+	$parent = $post->ID;
+}
+
+if ($post->post_parent = 313) {
 	include ($theme_location.'/views/components/nav/refugees.php');
 } else {
 	include ($theme_location.'/views/components/nav/main.php');
