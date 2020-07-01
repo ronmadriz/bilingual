8ef3e2756e2fcd0eb293e543de0d90855e27b22e
Ron Madriz <ron@firstcreative.com>
2020-07-01 12:17:04 -0500

    programs - overview loop

--- bei/views/components/galleries/rotating.php
+++ bei/views/components/galleries/rotating.php
@@ -2,8 +2,8 @@
 $ri_images = get_sub_field('ri_images');
 echo '<section id="rotating" class="rotating">'.PHP_EOL;
 echo '<div class="rotating__container container-fluid">'.PHP_EOL;
-echo '<div class="rotating__wrapper row">'.PHP_EOL;
-echo '<div id="rotating__content" class="rotating__content col-12 carousel slide carousel-fade" data-ride="carousel">'.PHP_EOL;
+echo '<div class="rotating__wrapper row justify-content-center">'.PHP_EOL;
+echo '<div id="rotating__content" class="rotating__content col-8 carousel slide carousel-fade" data-ride="carousel">'.PHP_EOL;
 if ($ri_images) {
 	echo '<div class="rotating__inner carousel-inner" role="listbox">'.PHP_EOL;
 	$ri_counter = 0;
