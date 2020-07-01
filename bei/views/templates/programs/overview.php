b76e3f196008a25d3fa87214191358001b4529e2
Ron Madriz <ron@firstcreative.com>
2020-07-01 10:48:41 -0500

    programs - overview loop

--- bei/views/templates/programs/overview.php
+++ bei/views/templates/programs/overview.php
@@ -13,15 +13,25 @@ $childprograms = new WP_Query($childrenArgs);
 
 include (get_template_directory().'/views/components/banner/subpages.php');
 if ($childprograms->have_posts()) {
-	echo '<section id="overview" class="overview">'.PHP_EOL;
+	echo '<section id="overview" class="tiled">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="overview__content col-12">'.PHP_EOL;//col-md-8
+	echo '<div class="row tiled__wrapper">'.PHP_EOL;
 	while ($childprograms->have_posts()) {
 		$childprograms->the_post();
-		echo '--hi--'.PHP_EOL;
+		$childprogram__img     = get_the_post_thumbnail_url($size = 'medium');
+		$childprogram__link    = get_the_permalink();
+		$childprogram__title   = get_the_title();
+		$childprogram__excerpt = get_the_excerpt();
+		echo '<figure class="tiled__item card col-12 col-md-3">'.PHP_EOL;
+		echo '<a href="'.$childprogram__link.'" class="tiled__image--link"><img src="'.esc_url($childprogram__img).'" alt="'.$childprogram__title['alt'].'" class="tiled__image card-img-top img-fluid"></a>'.PHP_EOL;
+		echo '<figcaption class="tiled__content">'.PHP_EOL;
+		echo (!empty($childprogram__title)?'<span class="tiled__title"><h3 class="tiled__title--text card-title">'.$childprogram__title.'</h3></span>'.PHP_EOL:'');
+		echo (!empty($childprogram__excerpt)?'<span class="tiled__desc card-text">'.$childprogram__excerpt.'</span>'.PHP_EOL:'');
+		echo (!empty($childprogram__link)?'<a href="'.$childprogram__link['url'].'" class="tiled__link">'.$childprogram__link['title'].' <span class="tiled__link--icon">'.file_get_contents(get_stylesheet_directory_uri().'/sprites/arrow.svg').'</span></a>'.PHP_EOL:'');
+		echo '</figcaption>'.PHP_EOL;
+		echo '</figure>'.PHP_EOL;
 	}
-	echo '</div>'.PHP_EOL;
+	wp_reset_postdata();
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
