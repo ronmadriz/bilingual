906993ed8c1b8fe2df3bac6d06372ec385b8d99c
Ron Madriz <ron@firstcreative.com>
2020-07-02 16:38:25 -0500

    testimonials - slider - css

--- bei/views/layout/html.php
+++ bei/views/layout/html.php
@@ -1,9 +1,10 @@
 <?
-$html_code = get_sub_field('html');
+$html_code = get_sub_field('add_html');
+
 echo '<section id="html_block" class="html_block">'.PHP_EOL;
 echo '<div class="container">'.PHP_EOL;
 echo '<div class="col-12">'.PHP_EOL;
-echo $html;
+echo $html_code;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
