3abefd1e6f41b3e0a6bb47ba58d9e04329f24b12
Ron Madriz <ron@firstcreative.com>
2020-07-02 14:41:59 -0500

    testimonials - slider

--- bei/views/components/testimonials/slider.php
+++ bei/views/components/testimonials/slider.php
@@ -26,17 +26,15 @@ if ($testimonial_Slider_posts) {
 		($testimonial_Slider_chunk === reset($testimonial_Slider_chunk))?$testimonial_active = "active":$testimonial_active = "";
 		$testimonial_Slider_html .= '<div class="item '.$testimonial_active.'">'.PHP_EOL;
 		foreach ($testimonial_Slider_chunk as $post) {
-
-			$testimonial__slider__image = get_the_post_thumbnail('thumbnail', array('class' => 'testimonial__image rounded-circle'));
-			$author_origin              = get_field('country_of_origin');
+			$author_origin = get_field('country_of_origin');
 
 			$testimonial_Slider_html .= '<figure class="testimonial__item col-12 col-md-4">'.PHP_EOL;
 			$testimonial_Slider_html .= '<blockquote class="testimonial__quote">'.PHP_EOL;
-			$testimonial_Slider_html .= get_the_content();
+			the_content();
 			$testimonial_Slider_html .= '</blockquote>'.PHP_EOL;
 			$testimonial_Slider_html .= '<footer class="testimonial__details">'.PHP_EOL;
 			$testimonial_Slider_html .= '<p class="testimonial__author"><cite class="testimonial__author--name">'.get_the_title().'</cite> <cite class="testimonial__author--country">('.$author_origin.')</cite></p>'.PHP_EOL;
-			$testimonial_Slider_html .= $testimonial__slider__image;
+			the_post_thumbnail('thumbnail', array('class' => 'testimonial__image rounded-circle'));
 			$testimonial_Slider_html .= '</footer>'.PHP_EOL;
 			$testimonial_Slider_html .= '</figure>'.PHP_EOL;
 		}
