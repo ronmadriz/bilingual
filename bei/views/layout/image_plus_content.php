e7e0daebf51f310e313908fa1b6cd2627b00bf81
Ron Madriz <ron@firstcreative.com>
2020-07-03 12:41:09 -0500

    image plus content

--- bei/views/layout/image_plus_content.php
+++ bei/views/layout/image_plus_content.php
@@ -1,28 +1,23 @@
 <?php
+$ipc_title   = get_sub_field('ipc_title');
+$ipc_content = get_sub_field('ipc_content');
+$ipc_image   = get_sub_field('ipc_image');
+$ipc_button  = get_sub_field('ipc_button');
+$ipc_size    = 'medium';
+$ipc_thumb   = $ipc_image['sizes'][$ipc_size];
 echo '<section id="ipc" class="ipc">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-while (have_rows('featured')) {
-	the_row();
-	$ipc_title   = get_sub_field('ipc_title');
-	$ipc_content = get_sub_field('ipc_content');
-	$ipc_image   = get_sub_field('ipc_image');
-	$ipc_button  = get_sub_field('ipc_button');
-	$ipc_size    = 'medium';
-	$ipc_thumb   = $ipc_image['sizes'][$ipc_size];
-
-	echo '<style type="text/css">'.PHP_EOL;
-	echo '@media(min-width:786px) {'.PHP_EOL;
-	echo '.featured__image {background-image:url('.$ipc_image['url'].')}'.PHP_EOL;
-	echo '}'.PHP_EOL;
-	echo '</style>'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
-	echo '<span class="featured__title">'.$ipc_title.'</span>'.PHP_EOL;
-	echo '<span class="featured__desc">'.$ipc_content.'</span>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '<div class="featured__image col-12 col-md-6 text-sm-center"><img src="'.esc_url($ipc_thumb).'" alt="" class="img-fluid d-md-none"></div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-}
-wp_reset_postdata();
+echo '<style type="text/css">'.PHP_EOL;
+echo '@media(min-width:786px) {'.PHP_EOL;
+echo '.featured__image {background-image:url('.$ipc_image['url'].')}'.PHP_EOL;
+echo '}'.PHP_EOL;
+echo '</style>'.PHP_EOL;
+echo '<div class="row">'.PHP_EOL;
+echo '<div class="featured__content col-12 col-md-6">'.PHP_EOL;
+echo '<span class="featured__title">'.$ipc_title.'</span>'.PHP_EOL;
+echo '<span class="featured__desc">'.$ipc_content.'</span>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '<div class="featured__image col-12 col-md-6 text-sm-center"><img src="'.esc_url($ipc_thumb).'" alt="" class="img-fluid d-md-none"></div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -6,6 +6,10 @@ include (get_template_directory().'/views/components/icons/four_icons.php');
 include ('single/tabs.php');
 include ('single/calendars.php');
 include ('single/schedules.php');
-if (have_rows('image_plus_content')) {;
-	include (get_template_directory().'/views/layout/image_plus_content.php');
+if (have_rows('image_plus_content')) {
+	while (have_rows('featured')) {
+		the_row();
+		include (get_template_directory().'/views/layout/image_plus_content.php');
+	}
+	wp_reset_postdata();
 }
