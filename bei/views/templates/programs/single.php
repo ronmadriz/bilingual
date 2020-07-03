f363d6cd752f8dacee4cd3784f0486be1f88cd22
Ron Madriz <ron@firstcreative.com>
2020-07-03 12:56:45 -0500

    image plus content

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -1,7 +1,7 @@
 <?php
 include (get_template_directory().'/views/components/banner/subpages.php');
 include (get_template_directory().'/views/layout/content-with-thumbnail.php');
-include ('blocks');
+include ('single/blocks.php');
 include (get_template_directory().'/views/components/icons/four_icons.php');
 include ('single/tabs.php');
 include ('single/calendars.php');
