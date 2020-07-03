8c398f3f08ca179a3f2139c88df0c8779de420fa
Ron Madriz <ron@firstcreative.com>
2020-07-03 15:40:44 -0500

    image plus content

--- bei/views/components/logos/flexible-logos.php
+++ bei/views/components/logos/flexible-logos.php
@@ -9,7 +9,7 @@ if (have_rows('logos')) {
 		the_row();
 		$fl_logo      = get_sub_field('logo_image');
 		$fl_logo_link = get_sub_field('logo_link');
-		echo '<div class="col-6 col-md-3 logos__item">';
+		echo '<div class="col logos__item">';
 		echo (!empty($fl_logo_link)?'<a href="'.$fl_logo_link['url'].'" rel="nofollow">':'');
 		echo '<img src="'.$fl_logo['url'].'" alt="'.$fl_logo['alt'].'" class="logos_image img-fluid"></div>'.PHP_EOL;
 		echo (!empty($fl_logo_link)?'</a>':'');
