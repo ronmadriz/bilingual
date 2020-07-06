c8600450c3a9d8b7bad48ad1f9562ca3e7cdfb17
Ron Madriz <ron@firstcreative.com>
2020-07-06 15:35:50 -0500

    content with form

--- bei/views/pages/home.php
+++ bei/views/pages/home.php
@@ -3,6 +3,5 @@
 include (get_template_directory().'/views/components/banner/home.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
 include (get_template_directory().'/views/components/icons/icons.php');
-include (get_template_directory().'/views/components/tabbed/vidTabs.php');
 include (get_template_directory().'/views/components/news/news-brief.php');
 include (get_template_directory().'/views/components/logos/logos.php');
