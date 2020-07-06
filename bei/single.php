7e751d6fcda8a24815c5bc6c319114f656c18089
Ron Madriz <ron@firstcreative.com>
2020-07-06 16:00:46 -0500

    content with form

---
+++ bei/single.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+$contentThumb = get_field('content_with_thumbnail');
+include ('views/components/banner/subpages.php');
+
+if ($contentThumb == 1) {
+	include ('views/pages/default.php');
+} else {
+	include ('views/layout/content-with-thumbnail.php');
+}
+
+include ('views/global/pagebuilder.php');
+get_footer();
