e299dafc75882217df8da6b7e75858905bbac57a
Ron Madriz <ron@firstcreative.com>
2020-07-06 14:20:16 -0500

    content with form

--- bei/views/layout/content-with-form.php
+++ bei/views/layout/content-with-form.php
@@ -2,8 +2,9 @@
 $cwf_title      = get_sub_field('cwf_title');
 $cwf_content    = get_sub_field('cwf_content');
 $cwf_form_title = get_sub_field('cwf_form_title');
-$cwf_form_raw   = get_sub_field('cwf_form', $post->ID);
-$cwf_form       = do_shortcode('[contact-form-7 id="'.$cwf_form_raw->id().'" title="'.$cwf_form_raw->title().'" html_id="cwf-form" html_class="form"]');
+$cwf_form_raw   = get_sub_field('cwf_form');
+$cwf_form       = do_shortcode($cwf_form_raw);
+// '[contact-form-7 id="'.$cwf_form_raw->id().'" title="'.$cwf_form_raw->title().'" html_id="cwf-form" html_class="form"]');
 
 echo '<section id="content-with-form" class="cwf twoColumns">'.PHP_EOL;
 echo '<div class="container-fluid twoColumns__wrapper">'.PHP_EOL;
