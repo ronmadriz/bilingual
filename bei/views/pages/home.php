82312992a85f1115faac210a4b44b56b49353487
Ron Madriz <ron@firstcreative.com>
2020-07-07 15:57:52 -0500

    homepage - news brief

--- bei/views/pages/home.php
+++ bei/views/pages/home.php
@@ -3,5 +3,6 @@
 include (get_template_directory().'/views/components/banner/home.php');
 include (get_template_directory().'/views/components/tabbed/home.php');
 include (get_template_directory().'/views/components/icons/icons.php');
+include (get_template_directory().'/views/components/tabbed/simple.php');
 include (get_template_directory().'/views/components/news/news-brief.php');
 include (get_template_directory().'/views/components/logos/logos.php');
