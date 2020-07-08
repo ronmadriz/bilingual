37769a065886f4a1c4a27f63b53e3fd98eb07407
Ron Madriz <ron@firstcreative.com>
2020-07-08 16:18:03 -0500

    documents link

--- bei/views/templates/programs/single.php
+++ bei/views/templates/programs/single.php
@@ -4,7 +4,7 @@ include (get_template_directory().'/views/layout/content-with-thumbnail.php');
 include ('single/blocks.php');
 include (get_template_directory().'/views/components/icons/four_icons.php');
 include ('single/tabs.php');
-include ('single/calendars.php');
+//include ('single/calendars.php');
 include ('single/schedules.php');
 if (have_rows('image_plus_content')) {
 	while (have_rows('image_plus_content')) {
