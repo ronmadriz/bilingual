5aa700952c04a2f664cf7f293dbcfab71b30b026
Ron Madriz <ron@firstcreative.com>
2020-07-08 13:56:39 -0500

    icons - fix

--- bei/views/components/documents/index.php
+++ bei/views/components/documents/index.php
@@ -2,41 +2,41 @@
 	<div class="container-fluid">
 		<div class="row documents__wrapper">
 			<div class="document__group col-12 col-md-6">
-				<h2>Group Title</h2>
-				<ul>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
+				<span class="section__title"><h2 class="section__title--text">Group Title</h2></span>
+				<ul class="document__list">
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
 				</ul>
 			</div>
 			<div class="document__group col-12 col-md-6">
-				<h2>Group Title</h2>
-				<ul>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
+				<span class="section__title"><h2 class="section__title--text">Group Title</h2></span>
+				<ul class="document__list">
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
 				</ul>
 			</div>
 			<div class="document__group col-12 col-md-6">
-				<h2>Group Title</h2>
-				<ul>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
+				<span class="section__title"><h2 class="section__title--text">Group Title</h2></span>
+				<ul class="document__list">
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
 				</ul>
 			</div>
 			<div class="document__group col-12 col-md-6">
-				<h2>Group Title</h2>
-				<ul>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
-					<li>icon - document title</li>
+				<span class="section__title"><h2 class="section__title--text">Group Title</h2></span>
+				<ul class="document__list">
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
+					<li class="document__item"><a href="#" class="document__link"><abbr title="file type" class="document__icon">icon</abbr> - document title</a></li>
 				</ul>
 			</div>
 		</div>
