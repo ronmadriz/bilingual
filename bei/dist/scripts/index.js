b09ea51ba45cce87c43a010e8cb55c6ced7f4335
Ron Madriz <ron@firstcreative.com>
2020-07-08 15:14:52 -0500

    icons - fix

--- bei/dist/scripts/index.js
+++ bei/dist/scripts/index.js
@@ -2,13 +2,26 @@ var j = jQuery.noConflict();
 
 j(document).ready(function(){
 
-	// ensure that mailto links are not treated as external links. Make external links open in a separate tab
-	j('a').not('[href*="mailto:"]').each(function () {
-		var isInternalLink = new RegExp('/' + window.location.host + '/');
-		if ( ! isInternalLink.test(this.href) ) {
-		  j(this).attr('target', '_blank');
-		};
-	});
+	if (j(window).width() > 769) {
+		j('.navbar .dropdown').hover(function() {
+			j(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+
+		}, function() {
+			j(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+
+		});
+
+		j('.navbar .dropdown > a').click(function() {
+			location.href = this.href;
+		});
+	}
+
+	j(('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href) && this.href.substr(0,4)!='tel:' && this.href.substr(0,7)!='mailto:') {
+            j(this).attr('target', '_blank');
+        }
+    });
 
 	// add lightbox to gallery 
 	j(document).on('click', '[data-toggle="lightbox"]', function(event) {
@@ -16,36 +29,6 @@ j(document).ready(function(){
 	    j(this).ekkoLightbox();
     });	
 
-});
 
-function doFormatPhone(A){
-	var B=document.getElementById(A);
-	B.onblur=function(){
-		formatPhone(this)
-	}
-}
-function formatPhone(A){
-	A.value=formatPhoneStr(A.value)
-}
-function formatPhoneStr(A){
-	var C=A.replace(/[^0-9xX]/g,"");
-	C=C.replace(/[xX]/g,"x");
-	var B="";
-	if(C.indexOf("x")>-1){
-		B=" "+C.substr(C.indexOf("x"));
-		C=C.substr(0,C.indexOf("x"))
-	}
-	switch(C.length){
-		case (10):
-		return C.replace(/(...)(...)(....)/g,"($1) $2-$3")+B;
-		case (11):
-		if(C.substr(0,1)=="1"){
-			return C.substr(1).replace(/(...)(...)(....)/g,"($1) $2-$3")+B
-		}
-		break;default:
-	}
-	return A
-}
-window.onload=function(){
-	doFormatPhone("phone")
-};
+
+});
--- bei/scripts/index.js
+++ bei/scripts/index.js
@@ -32,39 +32,3 @@ j(document).ready(function(){
 
 
 });
-
-
-
-
-
-function doFormatPhone(A){
-	var B=document.getElementById(A);
-	B.onblur=function(){
-		formatPhone(this)
-	}
-}
-function formatPhone(A){
-	A.value=formatPhoneStr(A.value)
-}
-function formatPhoneStr(A){
-	var C=A.replace(/[^0-9xX]/g,"");
-	C=C.replace(/[xX]/g,"x");
-	var B="";
-	if(C.indexOf("x")>-1){
-		B=" "+C.substr(C.indexOf("x"));
-		C=C.substr(0,C.indexOf("x"))
-	}
-	switch(C.length){
-		case (10):
-		return C.replace(/(...)(...)(....)/g,"($1) $2-$3")+B;
-		case (11):
-		if(C.substr(0,1)=="1"){
-			return C.substr(1).replace(/(...)(...)(....)/g,"($1) $2-$3")+B
-		}
-		break;default:
-	}
-	return A
-}
-window.onload=function(){
-	doFormatPhone("phone")
-};
