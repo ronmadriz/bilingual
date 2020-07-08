4d320e08d93f7654f1ff792d7f9897ef27573f4a
Ron Madriz <ron@firstcreative.com>
2020-07-08 15:12:24 -0500

    icons - fix

--- bei/views/components/documents/index.php
+++ bei/views/components/documents/index.php
@@ -10,10 +10,10 @@ if (have_rows('dc_group')) {
 		the_row();
 		$dc_title     = get_sub_field('dc_title');
 		$dc_documents = get_sub_field('dc_documents');
-		echo '<div class="document__group col-12 col-md-6">'.PHP_EOL;
-		echo '<span class="document__group__title"><h3 class="document__group__title--text">Group Title</h3></span>'.PHP_EOL;
+		echo '<div class="documents__group col-12 col-md-6">'.PHP_EOL;
+		echo '<span class="documents__group__title"><h3 class="documents__group__title--text">Group Title</h3></span>'.PHP_EOL;
 		if (have_rows('dc_documents')) {
-			echo '<ul class="document__list list-unstyled">'.PHP_EOL;
+			echo '<ul class="documents__list list-unstyled">'.PHP_EOL;
 			while (have_rows('dc_documents')) {
 				the_row();
 				$dc_document = get_sub_field('dc_document');
@@ -23,7 +23,7 @@ if (have_rows('dc_group')) {
 				$dc_file     = pathinfo($dc_url);
 				$dc_ext      = $dc_file['extension'];
 				$dc_icon     = file_get_contents(get_template_directory().'/sprites/'.$dc_ext.'.svg');
-				echo '<li class="document__item"><a href="'.esc_attr($dc_url).'" class="document__link"><abbr title="file type" class="document__icon">'.$dc_icon.'</abbr> - '.esc_html($dc_title).'</a></li>'.PHP_EOL;
+				echo '<li class="documents__item"><a href="'.esc_attr($dc_url).'" class="documents__link"><abbr title="file type" class="documents__icon">'.$dc_icon.'</abbr> - '.esc_html($dc_title).'</a></li>'.PHP_EOL;
 			}
 			wp_reset_postdata();
 			echo '</ul>'.PHP_EOL;
