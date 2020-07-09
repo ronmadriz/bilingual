e893c8d7b5f2d6ec0bdbfc8c8fd22169019ddec3
Ron Madriz <ron@firstcreative.com>
2020-07-09 16:35:55 -0500

    price page

--- bei/page-calendar.php
+++ bei/page-calendar.php
@@ -10,16 +10,4 @@ if ($contentThumb == 1) {
 	include ('views/layout/content-with-thumbnail.php');
 }
 
-$calendar_args = array(
-	// Type & Status Parameters
-	'post_type'      => 'programs',
-	'post_status'    => 'publish',
-	'order'          => 'DESC',
-	'orderby'        => 'date',
-	'posts_per_page' => 10,
-
-);
-
-$calendar_query = new WP_Query($calendar_args);
-
 get_footer();
--- bei/views/components/calendar/page.php
+++ bei/views/components/calendar/page.php
@@ -1,5 +1,4 @@
 <?php
-
 $cal_meta_query = [
 	[
 		'key'     => 'my_repeater_field_0_a_field_on_the_repeater',
@@ -9,9 +8,9 @@ $cal_meta_query = [
 
 $calendar_args = array(
 	// Type & Status Parameters
-	'post_type'      => 'programs',
-	'post_status'    => 'publish',
-	'meta_query'     => $cal_meta_query,
+	'post_type'   => 'programs',
+	'post_status' => 'publish',
+	// 'meta_query'     => $cal_meta_query,
 	'order'          => 'DESC',
 	'orderby'        => 'date',
 	'posts_per_page' => 10,
@@ -19,3 +18,10 @@ $calendar_args = array(
 );
 
 $calendar_query = new WP_Query($calendar_args);
+
+if ($calendar_query->have_posts()) {
+	while ($calendar_query->have_posts()) {
+		$calendar_query->the_post();
+		echo '<!-- hi -->'.PHP_EOL;
+	}
+}
