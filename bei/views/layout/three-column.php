43ca5a8c6f428d4b4f7231a2fc23580a63d1baf5
Ron Madriz <ron@firstcreative.com>
2020-07-09 16:18:17 -0500

    price page

--- bei/views/layout/three-column.php
+++ bei/views/layout/three-column.php
@@ -3,9 +3,9 @@ if (have_rows('three_column')) {
 	$column_count = 0;
 	while (have_rows('three_column')) {
 		the_row();
-		$column_one   = get_sub_field('column_one');
-		$column_two   = get_sub_field('column_two');
-		$column_three = get_sub_field('column_three');
+		$column_one   = get_sub_field('tc_column_one');
+		$column_two   = get_sub_field('tc_column_two');
+		$column_three = get_sub_field('tc_column_three');
 		echo '<section id="three_'.$column_count.'" class="three">'.PHP_EOL;
 		echo '<div class="container-fluid">'.PHP_EOL;
 		echo '<div class="row three__wrapper">'.PHP_EOL;
