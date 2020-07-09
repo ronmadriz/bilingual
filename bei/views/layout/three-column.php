f9428b35b800428359edb25c773957a3b9354965
Ron Madriz <ron@firstcreative.com>
2020-07-09 16:18:42 -0500

    price page

--- bei/views/layout/three-column.php
+++ bei/views/layout/three-column.php
@@ -1,20 +1,20 @@
 <?
 if (have_rows('three_column')) {
-	$column_count = 0;
+	$tc_column_count = 0;
 	while (have_rows('three_column')) {
 		the_row();
-		$column_one   = get_sub_field('tc_column_one');
-		$column_two   = get_sub_field('tc_column_two');
-		$column_three = get_sub_field('tc_column_three');
-		echo '<section id="three_'.$column_count.'" class="three">'.PHP_EOL;
+		$tc_column_one   = get_sub_field('tc_column_one');
+		$tc_column_two   = get_sub_field('tc_column_two');
+		$tc_column_three = get_sub_field('tc_column_three');
+		echo '<section id="three_'.$tc_column_count.'" class="three">'.PHP_EOL;
 		echo '<div class="container-fluid">'.PHP_EOL;
 		echo '<div class="row three__wrapper">'.PHP_EOL;
-		echo '<div class="three__item col-12 col-md-4">'.$column_one.'</div>'.PHP_EOL;
-		echo '<div class="three__item col-12 col-md-4">'.$column_two.'</div>'.PHP_EOL;
-		echo '<div class="three__item col-12 col-md-4">'.$column_three.'</div>'.PHP_EOL;
+		echo '<div class="three__item col-12 col-md-4">'.$tc_column_one.'</div>'.PHP_EOL;
+		echo '<div class="three__item col-12 col-md-4">'.$tc_column_two.'</div>'.PHP_EOL;
+		echo '<div class="three__item col-12 col-md-4">'.$tc_column_three.'</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 		echo '</section>'.PHP_EOL;
 	}
-	$column_count++;
+	$tc_column_count++;
 }
