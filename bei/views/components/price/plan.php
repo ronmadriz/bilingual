e4c8d8e55e28787fb65bb9d4829ce8545f0086f2
Ron Madriz <ron@firstcreative.com>
2020-07-09 12:26:29 -0500

    adjustment f1

---
+++ bei/views/components/price/plan.php
@@ -0,0 +1,17 @@
+<section id="plan" class="plan">
+	<div class="container-fluid">
+		<div class="row plan__wrapper">
+			<article class="plan__item">
+				<header class="plan__header"><h4 class="plan__header--text"></h4></header>
+				<span class="plan__cost"></span>
+				<ul class="plan__features">
+					<li class="plan__features--item">Feature</li>
+					<li class="plan__features--item">Feature</li>
+					<li class="plan__features--item">Feature</li>
+					<li class="plan__features--item">Feature</li>
+				</ul>
+				<footer class="plan__footer"><a href="#" class="plan__footer--link">Get Started!</a></footer>
+			</article>
+		</div>
+	</div>
+</section>
