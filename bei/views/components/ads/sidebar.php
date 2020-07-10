bd8031a2a1d6a85b91d575c44b632ae226555943
Ron Madriz <ron@firstcreative.com>
2020-07-10 10:12:15 -0500

    remove ads post type

--- bei/views/components/ads/sidebar.php
+++ bei/views/components/ads/sidebar.php
@@ -6,12 +6,12 @@ if (have_rows('sidebar_ads')) {
 		$ad_title   = get_sub_field('ad_title');
 		$ad_excerpt = get_sub_field('ad_excerpt');
 		$ad_link    = get_sub_field('ad_link');
-		$ad_img   = get_sub_field('ad_image');
-		$ad_size   = 'medium';
+		$ad_img     = get_sub_field('ad_image');
+		$ad_size    = 'medium';
 		$ad_image   = $ad_img['sizes'][$ad_size];
 		echo '<figure class="ad__wrapper">'.PHP_EOL;
 		echo '<figcaption>'.PHP_EOL;
-		echo '<a href="'.$ad_link['url'].'" class="ad__link"><img src="'.$ad_image.'" class="img-fluid ad__image"></a>'
+		echo '<a href="'.$ad_link['url'].'" class="ad__link"><img src="'.$ad_image.'" class="img-fluid ad__image"></a>'.PHP_EOL;
 		echo '<span class="ad__title"><h3 class="ad__title--txt">'.$ad_title.'</h3></span>'.PHP_EOL;
 		echo '<span class="ad__desc">'.$ad_excerpt.'</span>'.PHP_EOL;
 		echo '<span class="ad__button">'.PHP_EOL;
