3707e28890ecb222319dba6cd7d5ad9bab47c86c
Ron Madriz <ron@firstcreative.com>
2020-07-09 23:22:34 -0500

    calendar fix

--- bei/views/pages/default.php
+++ bei/views/pages/default.php
@@ -5,12 +5,7 @@ if (have_posts()) {
 	while (have_posts()) {
 		the_post();
 		echo '<div class="row"><div id="content" class="col-12">';
-		if (!is_404()) {
-			the_content();
-		} else {
-			echo '<h1>Oops!</h1>'.PHP_EOL;
-			echo '<p>Something seems to be wrong.</p>'.PHP_EOL;
-		}
+		the_content();
 		echo '</div></div>'.PHP_EOL;
 	}
 	wp_reset_postdata();
