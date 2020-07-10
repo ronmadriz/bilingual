1e36d29aa24dc434b071a3ab392061edad9338bf
Ron Madriz <ron@firstcreative.com>
2020-07-09 23:24:36 -0500

    calendar fix

--- bei/404.php
+++ bei/404.php
@@ -1,6 +1,5 @@
 <?php
 get_header();
-$contentThumb = get_field('content_with_thumbnail');
 include ('views/components/banner/subpages.php');
 include ('views/pages/default.php');
 get_footer();
---
+++ bei/page-our-promotions.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+$contentThumb = get_field('content_with_thumbnail');
+include ('views/components/banner/subpages.php');
+if ($contentThumb == 1) {
+	include ('views/pages/default.php');
+} else {
+	include ('views/layout/content-with-thumbnail.php');
+}
+echo '<!-- hi -->'.PHP_EOL;
+// include (get_template_directory().'/views/components/calendar/page.php');
+get_footer();
