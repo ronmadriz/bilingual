467fb2020eb9c0a2e02c04d6eaec4a5cb4c53e8c
Ron Madriz <ron@firstcreative.com>
2020-07-09 22:19:54 -0500

    calendar fix

--- bei/views/components/calendar/page.php
+++ bei/views/components/calendar/page.php
@@ -1,30 +1,43 @@
 <?php
-$cal_meta_query = [
-	[
-		'key'     => 'acf-field-5eea531878ce1',
-		'compare' => 'EXISTS',
-	]
-];
-
+/*
 $calendar_args = array(
-	// Type & Status Parameters
-	'post_type'      => 'programs',
-	'post_status'    => 'publish',
-	'meta_query'     => $cal_meta_query,
-	'order'          => 'DESC',
-	'orderby'        => 'date',
-	'posts_per_page' => 10,
+// Type & Status Parameters
+'post_type'   => 'programs',
+'post_status' => 'publish',
+'meta_query'  => array(
+'relation'   => 'AND',
+array(
+'key'     => 'calendars_section',
+'compare' => 'IN',
+)
+),
+'order'          => 'DESC',
+'orderby'        => 'date',
+'posts_per_page' => 10,
 
 );
 
 $calendar_query = new WP_Query($calendar_args);
 
 if ($calendar_query->have_posts()) {
-	while ($calendar_query->have_posts()) {
-		$calendar_query->the_post();
-		$got_calendar = get_field('calendars_section');
-		if (!empty($got_calendar)) {
-			echo '<!-- got em -->'.PHP_EOL;
-		}
+
+while ($calendar_query->have_posts()) {
+$calendar_query->the_post();
+$got_calendar = get_field('calendars_section');
+if (!empty($got_calendar)) {
+echo '<!-- got em -->'.PHP_EOL;
+}
+}
+ */
+
+$cal_posts = get_posts(array(
+		'posts_per_page' => -1,
+		'post_type'      => 'programs',
+	));
+
+if ($cal_posts) {
+	foreach ($cal_posts as $cal_post) {
+		setup_postdata($cal_post);
+		include (get_template_directory().'/views/components/tables/calendars.php');
 	}
 }
