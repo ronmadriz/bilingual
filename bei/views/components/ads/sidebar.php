0ace5b868efecc683d5e10246640e88fba0ff0d1
Ron Madriz <ron@firstcreative.com>
2020-07-10 10:31:04 -0500

    remove ads post type

--- bei/views/components/ads/sidebar.php
+++ bei/views/components/ads/sidebar.php
@@ -1,22 +1,24 @@
 <?php
+// do not set any of these to a class of ad(s) or advertisement(s) ad blockers will not show it
+
 if (have_rows('sidebar_ads')) {
-	echo '<div class="ad side">'.PHP_EOL;
-	echo '<div class="ad__wrapper">'.PHP_EOL;
+	echo '<div class="special side">'.PHP_EOL;
+	echo '<div class="special__wrapper">'.PHP_EOL;
 	while (have_rows('sidebar_ads')) {
 		the_row();
-		$ad_title   = get_sub_field('ad_title');
-		$ad_excerpt = get_sub_field('ad_excerpt');
-		$ad_link    = get_sub_field('ad_link');
-		$ad_img     = get_sub_field('ad_image');
-		$ad_size    = 'medium';
-		$ad_image   = $ad_img['sizes'][$ad_size];
-		echo '<figure class="ad__item">'.PHP_EOL;
-		echo '<a href="'.$ad_link['url'].'" class="ad__link"><img class="img-fluid ad__image" src="'.$ad_image.'"></a>'.PHP_EOL;
+		$special__title   = get_sub_field('ad_title');
+		$special__excerpt = get_sub_field('ad_excerpt');
+		$special__link    = get_sub_field('ad_link');
+		$special__img     = get_sub_field('ad_image');
+		$special__size    = 'medium';
+		$special__image   = $special__img['sizes'][$special__size];
+		echo '<figure class="special__item">'.PHP_EOL;
+		echo '<a href="'.$special__link['url'].'" class="special__link"><img class="img-fluid special__image" src="'.$special__image.'"></a>'.PHP_EOL;
 		echo '<figcaption>'.PHP_EOL;
-		echo '<span class="ad__title"><h3 class="ad__title--txt">'.$ad_title.'</h3></span>'.PHP_EOL;
-		echo '<span class="ad__desc">'.$ad_excerpt.'</span>'.PHP_EOL;
-		echo '<span class="ad__button">'.PHP_EOL;
-		echo '<a href="'.$ad_link['url'].'" class="ad__link">'.$ad_link['title'].'</a>'.PHP_EOL;
+		echo '<span class="special__title"><h3 class="special__title--txt">'.$special__title.'</h3></span>'.PHP_EOL;
+		echo '<span class="special__desc">'.$special__excerpt.'</span>'.PHP_EOL;
+		echo '<span class="special__button">'.PHP_EOL;
+		echo '<a href="'.$special__link['url'].'" class="special__link">'.$special__link['title'].'</a>'.PHP_EOL;
 		echo '</span>'.PHP_EOL;
 		echo '</figcaption>'.PHP_EOL;
 		echo '</figure>'.PHP_EOL;
