8d6479c6315149845fe4a853dd855bcd991b695a
Ron Madriz <ron@firstcreative.com>
2020-07-10 09:40:20 -0500

    remove ads post type

--- bei/views/components/ads/square.php
+++ bei/views/components/ads/square.php
@@ -1,11 +1,15 @@
-<div class="ad side">
-	<figure class="ad__wrapper">
-		<figcaption>
-			<span class="ad__title"><h3 class="ad__title--txt"></h3></span>
-			<span class="ad__desc"></span>
-			<span class="ad__button">
-				<a href="#" class="ad__link">Read More</a>
-			</span>
-		</figcaption>
-	</figure>
-</div>
+<?php
+
+echo '<div class="ad side">'.PHP_EOL;
+echo '<figure class="ad__wrapper">'.PHP_EOL;
+echo '<figcaption>'.PHP_EOL;
+echo '<span class="ad__title"><h3 class="ad__title--txt">'.get_the_title().'</h3></span>'.PHP_EOL;
+echo '<span class="ad__desc">'.PHP_EOL;
+the_excerpt();
+echo '</span>'.PHP_EOL;
+echo '<span class="ad__button">'.PHP_EOL;
+echo '<a href="#" class="ad__link">Learn More</a>'.PHP_EOL;
+echo '</span>'.PHP_EOL;
+echo '</figcaption>'.PHP_EOL;
+echo '</figure>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
