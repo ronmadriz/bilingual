0831c33a2e68be6c4297a8bcab79260a0b990c1e
Ron Madriz <ron@firstcreative.com>
2020-07-09 22:38:38 -0500

    calendar fix

--- bei/views/components/calendar/page.php
+++ bei/views/components/calendar/page.php
@@ -29,9 +29,50 @@ if ($calendar_query->have_posts()) {
 				echo '<section id="calendar" class="calendar">'.PHP_EOL;
 				echo '<div class="container-fluid">'.PHP_EOL;
 				echo (!empty($calendar_section_title)?'<div class="row justify-content-end"><div class="col-12 calendar__title"><h2>'.$calendar_section_title.'</h2></div></div>'.PHP_EOL:'');
-				if (have_rows('calendars')) {
+				if (have_rows('calendars', $cal_post_id)) {
 					echo '<div class="row">'.PHP_EOL;
-
+					while (have_rows('calendars', $cal_post_id)) {
+						the_row();
+						$calendar_title = get_sub_field('calendar_title', $cal_post_id);
+						echo '<div class="calendar__wrapper col-12 col-md-6">'.PHP_EOL;
+						echo (!empty($calendar_title)?'<h3 class="calendar__label">'.$calendar_title.'</h3>'.PHP_EOL:'');
+						echo '<table class="table calendar__table">'.PHP_EOL;
+						echo '<thead>'.PHP_EOL;
+						echo '<tr>'.PHP_EOL;
+						echo '<th class="calendar__headers">'.PHP_EOL;
+						_e('Cycle # &amp; Year', 'bei_core');
+						echo '</th>'.PHP_EOL;
+						echo '<th class="calendar__headers">'.PHP_EOL;
+						_e('Start Date', 'bei_core');
+						echo '</th>'.PHP_EOL;
+						echo '<th class="calendar__headers">'.PHP_EOL;
+						_e('End Date', 'bei_core');
+						echo '</th></tr>'.PHP_EOL;
+						echo '</thead>'.PHP_EOL;
+						if (have_rows('calendar')) {
+							echo '<tbody>'.PHP_EOL;
+							while (have_rows('calendar')) {
+								the_row();
+								$cycle_info  = get_sub_field('cycle', $cal_post_id);
+								$cycle_start = get_sub_field('start', $cal_post_id);
+								$cycle_end   = get_sub_field('end', $cal_post_id);
+								echo '<tr class="calendar__row">'.PHP_EOL;
+								echo '<td class="calendar__data">'.(!empty($cycle_info)?$cycle_info:'').'</td>'.PHP_EOL;
+								echo '<td class="calendar__data" data-label="';
+								_e('Start Date', 'bei_core');
+								echo '">'.(!empty($cycle_start)?$cycle_start:'').'</td>'.PHP_EOL;
+								echo '<td class="calendar__data" data-label="';
+								_e('End Date', 'bei_core');
+								echo '">'.(!empty($cycle_end)?$cycle_end:'').'</td>'.PHP_EOL;
+								echo '</tr>'.PHP_EOL;
+							}
+							wp_reset_postdata();
+							echo '</tbody>'.PHP_EOL;
+						}
+						echo '</table>'.PHP_EOL;
+						echo '</div>'.PHP_EOL;
+					}
+					wp_reset_postdata();
 					echo '</div>'.PHP_EOL;
 				}
 				echo '</div>'.PHP_EOL;
