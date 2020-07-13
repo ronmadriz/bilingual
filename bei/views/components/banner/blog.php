30c00baaedd1a921e7e6678fc4b625cfb706d451
Ron Madriz <ron@firstcreative.com>
2020-07-13 14:21:26 -0500

    homepage fix blog posts

--- bei/views/components/banner/blog.php
+++ bei/views/components/banner/blog.php
@@ -1,11 +1,9 @@
 <?php
-$blog_post = get_the_ID();
-
 echo '<section id="subbanner" class="subbanner">'.PHP_EOL;
 echo '<div class="container-fluid subbanner__wrapper">'.PHP_EOL;
 echo '<div class="row subbanner__row align-content-center">'.PHP_EOL;
 echo '<div class="col-12 subbanner__content">'.PHP_EOL;
-echo '<h1 class="subbanner__title">'.get_the_title($blog_post).'</h1>'.PHP_EOL;
+echo '<h1 class="subbanner__title">'.single_post_title().'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
