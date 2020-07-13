92251c87a23d0ec2281415e3f56699f404c299d2
Ron Madriz <ron@firstcreative.com>
2020-07-13 16:39:56 -0500

    search - fix

--- bei/search.php
+++ bei/search.php
@@ -12,7 +12,7 @@ $contentThumb = get_field('content_with_thumbnail');
 include ('views/components/banner/subpages.php');
 echo '<section id="main-content" class="main-content">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row"><div id="content" class="col-12">';
+echo '<div class="row"><div id="content" class="col-12 align-content-center justify-content-center">';
 get_search_form();
 echo '</div></div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
