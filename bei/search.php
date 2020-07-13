3b1c7477a1c3d42d5ca6a88725895bc45f06e777
Ron Madriz <ron@firstcreative.com>
2020-07-13 16:05:32 -0500

    home page - icons

---
+++ bei/search.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+$contentThumb = get_field('content_with_thumbnail');
+include ('views/components/banner/subpages.php');
+echo '<section id="main-content" class="main-content">'.PHP_EOL;
+echo '<div class="container-fluid">'.PHP_EOL;
+echo '<div class="row"><div id="content" class="col-12">';
+get_search_form();
+echo '</div></div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
+get_footer();
