7a4eabb1223d0426b6e58a603e75963e2817ce30
Ron Madriz <ron@firstcreative.com>
2020-07-13 16:41:36 -0500

    search - fix

--- bei/search.php
+++ bei/search.php
@@ -14,7 +14,7 @@ echo '<div class="container-fluid subbanner__wrapper">'.PHP_EOL;
 echo '<div class="row subbanner__row align-content-center">'.PHP_EOL;
 echo '<div class="col-12 subbanner__content">'.PHP_EOL;
 echo '<h1 class="subbanner__title">';
-single_post_title()
+single_post_title();
 echo '</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
