6399438a0ce6612981eaeec73985dc1f3cd9d0a6
Ron Madriz <ron@firstcreative.com>
2020-07-13 14:11:27 -0500

    search item removal for now

--- bei/views/layout/blog-home.php
+++ bei/views/layout/blog-home.php
@@ -1,13 +1,12 @@
 <?php
 $args = array(
-	'category__not_in' => array( 1, 2 ),
-	'post_type' => 'post',
-	'post_status' => 'publish',
-	'paged'                  => get_query_var( 'paged' ),
+	'category__not_in' => array(1, 2),
+	'post_type'        => 'post',
+	'post_status'      => 'publish',
+	'paged'            => get_query_var('paged'),
 );
 
-$query = new WP_Query( $args );
-
+$query = new WP_Query($args);
 
 echo '<section id="main" class="main news">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
@@ -21,12 +20,20 @@ if (have_posts()) {
 		the_post();
 		$news__itemID = get_the_id();
 		echo '<article class="col-12 news__item">'.PHP_EOL;
-		echo '<header class="news__header">'.PHP_EOL;
-		the_post_thumbnail($news__itemID, 'post-thumbnail', ['class' => 'img-responsive responsive--full'] );
+		echo '<header class="news__header">';
+		echo '<span class="news__image"><a href="'.get_the_permalink($news__itemID).' class="news__image--link">'.PHP_EOL;
+		the_post_thumbnail($news__itemID, 'post-thumbnail', ['class' => 'img-responsive news__image--img']);
+		echo '</a></span>'.PHP_EOL;
+		echo '<span class="news__title"><h3 class="news__title--text"><a href="'.get_the_permalink($news__itemID).' class="news__title--link">';
+		the_title($news__itemID);
+		echo '</a></h3></span>'.PHP_EOL;
 		echo '</header>'.PHP_EOL;
+		echo '<span class="news__content">';
+		the_excerpt();
+		echo '</span>'.PHP_EOL;
 		echo '<footer class="news__footer buttons">'.PHP_EOL;
-		echo '<a href="'.get_the_permalink($news__itemID).'>';
-		_e('Read More', 'bei_core')
+		echo '<a href="'.get_the_permalink($news__itemID).' class="news__link">';
+		_e('Read More', 'bei_core');
 		echo '</a>'.PHP_EOL;
 		echo '</footer>'.PHP_EOL;
 		echo '</article>'.PHP_EOL;
