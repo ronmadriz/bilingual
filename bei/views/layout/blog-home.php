9e6c201abe3cfc2d45775c0df060af8ae7784a44
Ron Madriz <ron@firstcreative.com>
2020-07-13 14:13:42 -0500

    search item removal for now

--- bei/views/layout/blog-home.php
+++ bei/views/layout/blog-home.php
@@ -18,13 +18,12 @@ if (have_posts()) {
 	echo '<div class="row news__wrapper">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
-		$news__itemID = get_the_id();
 		echo '<article class="col-12 d-block news__item">'.PHP_EOL;
 		echo '<header class="news__header">';
-		echo '<span class="news__image"><a href="'.get_the_permalink($news__itemID).' class="news__image--link">'.PHP_EOL;
-		the_post_thumbnail($news__itemID, 'medium', ['class' => 'img-fluid news__image--img']);
+		echo '<span class="news__image"><a href="'.get_the_permalink().' class="news__image--link">'.PHP_EOL;
+		the_post_thumbnail('medium', ['class' => 'img-fluid news__image--img']);
 		echo '</a></span>'.PHP_EOL;
-		echo '<span class="news__title"><h3 class="news__title--text"><a href="'.get_the_permalink($news__itemID).' class="news__title--link">';
+		echo '<span class="news__title"><h3 class="news__title--text"><a href="'.get_the_permalink().' class="news__title--link">';
 		the_title();
 		echo '</a></h3></span>'.PHP_EOL;
 		echo '</header>'.PHP_EOL;
@@ -32,7 +31,7 @@ if (have_posts()) {
 		the_excerpt();
 		echo '</span>'.PHP_EOL;
 		echo '<footer class="news__footer buttons">'.PHP_EOL;
-		echo '<a href="'.get_the_permalink($news__itemID).' class="news__link">';
+		echo '<a href="'.get_the_permalink().' class="news__link">';
 		_e('Read More', 'bei_core');
 		echo '</a>'.PHP_EOL;
 		echo '</footer>'.PHP_EOL;
