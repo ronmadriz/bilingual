fbcdfbd5f49b22f54b984b6c02ec77e5f4b1c955
Ron Madriz <ron@firstcreative.com>
2020-07-13 16:34:49 -0500

    search - fix

--- bei/search.php
+++ bei/search.php
@@ -17,4 +17,34 @@ get_search_form();
 echo '</div></div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
+
+echo '<section id="results" class="results">'.PHP_EOL;
+echo '<div class="container">'.PHP_EOL;
+echo '<div class="row results__wrapper">'.PHP_EOL;
+echo '<div class="col-12 section__title">'.PHP_EOL;
+echo '<h2 class="section__title--text">Displaying Search Results For: ';
+printf(esc_html__('%s', 'webdevors'), '<span>'.get_search_query().'</span>');
+echo '</h2>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+if (have_posts()) {
+	echo '<div class="row justify-content-center align-content-center results__content mb-2">'.PHP_EOL;
+	while (have_posts()) {
+		the_post();
+		echo '<div class="results__item col-12 mt-1 mb-1">'.PHP_EOL;
+		echo '<h2 class="results__title"><a class="results__link" href="'.get_the_permalink().'" rel="bookmark">'.get_the_title().'</a></h2>'.PHP_EOL;
+		echo '<div class="results__summary">'.PHP_EOL;
+		the_excerpt();
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
+} else {
+	echo '<div class="col-12 results__item">'.PHP_EOL;
+	echo '<p class="results__message">';
+	_e('Not what you&apos;re looking for? Start a new search:', 'bei_core');
+	echo '</p>'.PHP_EOL;
+	get_search_form();
+	echo '</div>'.PHP_EOL;
+}
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
 get_footer();
