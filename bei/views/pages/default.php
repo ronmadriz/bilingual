32646bba86c8f03a8860d1ab38b134de43d55cfd
Ron Madriz <ron@firstcreative.com>
2020-07-15 12:41:48 -0500

    accordion - description field added

--- bei/views/pages/default.php
+++ bei/views/pages/default.php
@@ -4,6 +4,11 @@ if (have_posts()) {
 	echo '<div class="container-fluid">'.PHP_EOL;
 	while (have_posts()) {
 		the_post();
+		echo '<div class="row"><div class="breadcrumbs__content">'.PHP_EOL;
+		if (!is_home()) {
+			the_breadcrumb();
+		}
+		echo '</div></div>'.PHP_EOL;
 		echo '<div class="row"><div id="content" class="col-12">';
 		the_content();
 		echo '</div></div>'.PHP_EOL;
