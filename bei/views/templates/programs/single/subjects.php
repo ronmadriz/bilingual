d1ff75e54065d8cc4d942a19c7a77501899532d6
Ron Madriz <ron@firstcreative.com>
2020-07-28 19:25:05 -0500

    subjects title

--- bei/views/templates/programs/single/subjects.php
+++ bei/views/templates/programs/single/subjects.php
@@ -1,10 +1,9 @@
 <?php
 if (have_rows('subjects')) {
+	$fi_section_title = get_field('fi_section_title');
 	echo '<section id="subjects" class="subjects">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row"><div class="col-12 subjects__section"><h2>';
-	_e('Core Subjects', 'bei_core');
-	echo '</h2></div></div>'.PHP_EOL;
+	echo '<div class="row"><div class="col-12 subjects__section"><h2>'.$fi_section_title.'</h2></div></div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
 	include (get_template_directory().'/views/components/icons/four_icons.php');
 	echo '</div>'.PHP_EOL;
